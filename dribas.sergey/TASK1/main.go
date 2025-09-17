3b65b69e2ed22cb4c9f17063e8f0f6ca9200cf00
Rassoha <serdribas@gmail.com>
2025-09-17 18:41:06 +0300

    [TASK-1] Release all basic operations like + - * /

--- dribas.sergey/TASK1/main.go
+++ dribas.sergey/TASK1/main.go
@@ -1,5 +1,45 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var (
+		leftOperand  int
+		rightOperand int
+		operation    string
+		result       int
+	)
+
+	n, err := fmt.Scan(&leftOperand, &rightOperand)
+
+	if err != nil {
+		if n == 0 {
+			fmt.Println("Invalid first operand")
+		} else if n == 1 {
+			fmt.Println("Invalid second operand")
+		}
+		return
+	}
+
+	fmt.Scan(&operation)
+
+	switch operation {
+	case "+":
+		result = leftOperand + rightOperand
+	case "-":
+		result = leftOperand - rightOperand
+	case "*":
+		result = leftOperand * rightOperand
+	case "/":
+		if rightOperand == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		result = leftOperand / rightOperand
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+	fmt.Println(result)
 }
