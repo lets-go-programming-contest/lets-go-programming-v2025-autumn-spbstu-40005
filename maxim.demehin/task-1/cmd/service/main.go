07c5b63c85b5e9d15386556fc7129e68749098a4
TvoyBatyA12343 <maxdem06@gmail.com>
2025-09-17 20:15:04 +0300

    [TASK-1] change errors handling

--- maxim.demehin/task-1/cmd/service/main.go
+++ maxim.demehin/task-1/cmd/service/main.go
@@ -6,38 +6,37 @@ import (
 
 func main() {
 	var op1, op2 int
-	_, err1 := fmt.Scan(&op1)
-	_, err2 := fmt.Scan(&op2)
-
-	if err1 != nil {
+	_, err := fmt.Scan(&op1)
+	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
-	if err2 != nil {
+
+	_, err = fmt.Scan(&op2)
+	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
 
 	var operation string
-	_, _ = fmt.Scan(&operation)
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Operation input error")
+	}
 
 	switch operation {
 	case "+":
 		fmt.Println(op1 + op2)
-
 	case "-":
 		fmt.Println(op1 - op2)
-
 	case "*":
 		fmt.Println(op1 * op2)
-
 	case "/":
 		if op2 == 0 {
 			fmt.Println("Division by zero")
 			return
 		}
 		fmt.Println(op1 / op2)
-
 	default:
 		fmt.Println("Invalid operation")
 	}
