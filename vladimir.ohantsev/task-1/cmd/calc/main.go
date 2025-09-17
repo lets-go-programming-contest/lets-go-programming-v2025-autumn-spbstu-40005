0685ffd9c236b081f2b943de4d62cccbe8a550cf
P3rCh1 <okhantsev.vova@mail.ru>
2025-09-17 19:12:41 +0300

    [TASK-1] Initial commit

---
+++ vladimir.ohantsev/task-1/cmd/calc/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func plus(x, y int) (int, error) {
+	return x + y, nil
+}
+
+func minus(x, y int) (int, error) {
+	return x - y, nil
+}
+
+func mult(x, y int) (int, error) {
+	return x * y, nil
+}
+
+func div(x, y int) (int, error) {
+	if y == 0 {
+		return 0, errors.New("Division by zero")
+	}
+	return x / y, nil
+}
+
+func main() {
+	m := map[string]func(x, y int) (int, error){
+		"+": plus,
+		"-": minus,
+		"*": mult,
+		"/": div,
+	}
+	var x, y int
+	var op string
+	if _, err := fmt.Scan(&x); err != nil {
+		fmt.Fprintln(os.Stderr, "Invalid first operand")
+		os.Exit(1)
+	}
+	if _, err := fmt.Scan(&y); err != nil {
+		fmt.Fprintln(os.Stderr, "Invalid second operand")
+		os.Exit(1)
+	}
+	fmt.Scan(&op)
+	if f, ok := m[op]; ok {
+		res, err := f(x, y)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(res)
+	} else {
+		fmt.Fprintln(os.Stderr, "Invalid operation")
+		os.Exit(1)
+	}
+}
---
+++ vladimir.ohantsev/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/lets-go-programming-contest/lets-go-programming-v2025-autumn-spbstu-40005/vladimir.ohantsev/task-1
+
+go 1.22
