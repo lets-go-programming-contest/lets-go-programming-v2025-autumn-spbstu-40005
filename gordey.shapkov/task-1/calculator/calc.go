f3eb564ba0167a6a6e1ada82e8aebdd40c2d691a
TheEmfield <emfield337@gmail.com>
2025-09-17 20:45:37 +0300

    [TASK-1] format changes for lint

--- gordey.shapkov/task-1/calculator/calc.go
+++ gordey.shapkov/task-1/calculator/calc.go
@@ -3,20 +3,20 @@ package calculator
 import "errors"
 
 func Calc(a int, b int, op string) (int, error) {
-  switch op {
-    case "+":
-      return a + b, nil
-    case "-":
-      return a - b, nil
-    case "*":
-      return a * b, nil
-    case "/":
-      if b == 0 {
-        return 0, errors.New("Division by zero")
-      } else {
-        return a / b, nil
-      }
-    default:
-      return 0, errors.New("Invalid operation")
-  }
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, errors.New("Division by zero")
+		} else {
+			return a / b, nil
+		}
+	default:
+		return 0, errors.New("Invalid operation")
+	}
 }
--- gordey.shapkov/task-1/cmd/service/main.go
+++ gordey.shapkov/task-1/cmd/service/main.go
@@ -1,32 +1,34 @@
 package main
 
-import ("fmt"
-        "gordey.shapkov/task-1/calculator")
+import (
+	"fmt"
+	"gordey.shapkov/task-1/calculator"
+)
 
 func main() {
-  var (
-      a, b int
-      op string
-      )
-  _, err := fmt.Scan(&a)
-  if err != nil {
-    fmt.Println("Invalid first operand")
-    return
-  }
-  _, err = fmt.Scan(&b)
-  if err != nil {
-    fmt.Println("Invalid second operand")
-    return
-  }
-  _, err = fmt.Scan(&op)
-  if err != nil {
-    fmt.Println("Invalid operator")
-    return
-  }
-  res, err := calculator.Calc(a, b, op)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  fmt.Println(res)
+	var (
+		a, b int
+		op   string
+	)
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operator")
+		return
+	}
+	res, err := calculator.Calc(a, b, op)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res)
 }
