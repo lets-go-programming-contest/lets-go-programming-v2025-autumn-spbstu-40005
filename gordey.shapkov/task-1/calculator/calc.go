f399173dfe1fd3e8a8edb853615d9465ab8fd93b
TheEmfield <emfield337@gmail.com>
2025-09-18 19:21:57 +0300

    [TASK-1] static errors in calc.go and new go version

--- gordey.shapkov/task-1/calculator/calc.go
+++ gordey.shapkov/task-1/calculator/calc.go
@@ -2,6 +2,11 @@ package calculator
 
 import "errors"
 
+var (
+	errDivisionByZero   = errors.New("Division by zero")
+	errInvalidOperation = errors.New("Invalid operation")
+)
+
 func Calc(a int, b int, op string) (int, error) {
 	switch op {
 	case "+":
@@ -12,11 +17,11 @@ func Calc(a int, b int, op string) (int, error) {
 		return a * b, nil
 	case "/":
 		if b == 0 {
-			return 0, errors.New("Division by zero")
+			return 0, errDivisionByZero
 		} else {
 			return a / b, nil
 		}
 	default:
-		return 0, errors.New("Invalid operation")
+		return 0, errInvalidOperation
 	}
 }
--- gordey.shapkov/task-1/go.mod
+++ gordey.shapkov/task-1/go.mod
@@ -1,3 +1,3 @@
 module gordey.shapkov/task-1
 
-go 1.22.2
+go 1.22.7
