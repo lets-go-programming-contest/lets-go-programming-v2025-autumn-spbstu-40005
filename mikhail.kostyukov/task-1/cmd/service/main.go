b45b2a63d585d06c581d064732bedef1244e1f5a
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-09-20 17:58:03 +0300

    [TASK-1] add input reading

--- mikhail.kostyukov/task-1/cmd/service/main.go
+++ mikhail.kostyukov/task-1/cmd/service/main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("init stage")
+	var (
+		operand1, operand2	int
+		operation			string
+	)
+	_, err := fmt.Scan(&operand1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operand2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 }
