198941b5bd04d269d5097f24ba3c7602f1d4fb84
P3rCh1 <okhantsev.vova@mail.ru>
2025-09-21 02:40:20 +0300

    [TASK-2-2] first working

--- vladimir.ohantsev/task-2-2/cmd/service/main.go
+++ vladimir.ohantsev/task-2-2/cmd/service/main.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"container/heap"
+	"errors"
+	"fmt"
+)
 
+var (
+	errInputFail      = errors.New("input error")
+	errInvalidRequest = errors.New("invalid request")
+)
+
+type intHeap []int
+
+func (h *intHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *intHeap) Pop() any {
+	val := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+
+	return val
+}
+
+func (h *intHeap) Len() int           { return len(*h) }
+func (h *intHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *intHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func main() {
+	var dishes int
+	if _, err := fmt.Scan(&dishes); err != nil || dishes <= 0 {
+		fmt.Println(errInputFail.Error())
+		return
+	}
+
+	dishesHeap := &intHeap{}
+
+	for range dishes {
+		var cost int
+		if _, err := fmt.Scan(&cost); err != nil {
+			fmt.Println(errInputFail.Error())
+			return
+		}
+
+		heap.Push(dishesHeap, cost)
+	}
+
+	var need int
+	if _, err := fmt.Scan(&need); err != nil || need <= 0 {
+		fmt.Println(errInputFail.Error())
+		return
+	}
+
+	if need > dishesHeap.Len() {
+		fmt.Println(errInvalidRequest.Error())
+		return
+	}
+
+	for range need - 1 {
+		heap.Pop(dishesHeap)
+	}
+
+	fmt.Println(heap.Pop(dishesHeap).(int))
 }
