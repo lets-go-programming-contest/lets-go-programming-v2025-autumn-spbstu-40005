006d1d0975860ed9ab692f4de55b4839e4491307
TheEmfield <emfield337@gmail.com>
2025-09-22 15:11:12 +0300

    [TASK-2-1] most workable version

--- gordey.shapkov/task-2-1/cmd/service/main.go
+++ gordey.shapkov/task-2-1/cmd/service/main.go
@@ -1,121 +1,101 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
-const (
-	MaxTemp = 30
-	MinTemp = 15
-)
-
-var (
-	errInput = errors.New("invalid input")
-	errSign  = errors.New("invalid operator")
-)
-
-func computeCurrTemp(currTemp, minTemp, maxTemp, preferredTemp int, sign string) int {
-	switch sign {
-	case ">=":
-		if preferredTemp > maxTemp {
-			return -1
-		} else if preferredTemp > currTemp {
-			currTemp = preferredTemp
-		}
-		if preferredTemp > minTemp {
-			minTemp = preferredTemp
-		}
-	case "<=":
-		if preferredTemp < minTemp {
-			return -1
-		} else if preferredTemp < currTemp {
-			currTemp = preferredTemp
-		}
-		if preferredTemp < maxTemp {
-			maxTemp = preferredTemp
-		}
-	default:
-		return -1
+func main() {
+	var number, count int
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		return
 	}
-	return currTemp
+
+	for idx := 0; idx < number; idx++ {
+		_, err = fmt.Scan(&count)
+		if err != nil {
+			return
+		}
+
+		var (
+			sign        string
+			preferedTemp int
+			preferences  []string
+		)
+
+		for innerIdx := 0; innerIdx < count; innerIdx++ {
+			_, err = fmt.Scan(&sign, &preferedTemp)
+			if err != nil {
+				return
+			}
+
+			temp := sign + " " + strconv.Itoa(preferedTemp)
+			preferences = append(preferences, temp)
+		}
+
+		changeTemperature(preferences)
+	}
 }
 
 func changeTemperature(preferences []string) {
+	const (
+		MaxTemp = 30
+		MinTemp = 15
+	)
+
 	maxTemp := MaxTemp
 	minTemp := MinTemp
 	currTemp := 0
 
-	if len(preferences) > 0 {
-		parts := strings.Fields(preferences[0])
-		preferredTemp, _ := strconv.Atoi(parts[1])
-		sign := parts[0]
-
-		if preferredTemp < MinTemp || preferredTemp > MaxTemp {
-			currTemp = -1
-		} else {
-			if sign == ">=" {
-				currTemp = preferredTemp
-			} else {
-				currTemp = MinTemp
-			}
-		}
-		fmt.Println(currTemp)
-		minTemp = preferredTemp
-		maxTemp = preferredTemp
-	}
-
-	for idx := 1; idx < len(preferences); idx++ {
+	for idx := 0; idx < len(preferences); idx++ {
 		parts := strings.Fields(preferences[idx])
-		preferredTemp, _ := strconv.Atoi(parts[1])
+		preferedTemp, _ := strconv.Atoi(parts[1])
 		sign := parts[0]
 
-		if preferredTemp < MinTemp || preferredTemp > MaxTemp {
+		if preferedTemp < MinTemp || preferedTemp > MaxTemp {
 			currTemp = -1
 		} else {
-			currTemp = computeCurrTemp(currTemp, minTemp, maxTemp, preferredTemp, sign)
+			if idx == 0 {
+				if sign == ">=" {
+					currTemp = preferedTemp
+				} else {
+					currTemp = MinTemp
+				}
+			}
+
+			switch sign {
+			case ">=":
+				if preferedTemp > maxTemp {
+					currTemp = -1
+				} else if preferedTemp > currTemp {
+					currTemp = preferedTemp
+				}
+				if preferedTemp > minTemp {
+					minTemp = preferedTemp
+				}
+			case "<=":
+				if preferedTemp < minTemp {
+					currTemp = -1
+				} else if preferedTemp < currTemp {
+					currTemp = preferedTemp
+				}
+				if preferedTemp < maxTemp {
+					maxTemp = preferedTemp
+				}
+			default:
+				currTemp = -1
+			}
 		}
 
 		if currTemp == -1 {
-			for empIdx := idx; empIdx < len(preferences); empIdx++ {
+			for innerIdx := idx; innerIdx < len(preferences); innerIdx++ {
 				fmt.Println(currTemp)
 			}
 			return
 		}
+
 		fmt.Println(currTemp)
-	}
-}
-
-func main() {
-	var countDeparts int
-
-	if _, err := fmt.Scan(&countDeparts); err != nil || countDeparts < 1 {
-		fmt.Println(errInput.Error())
-		return
-	}
-
-	for departIdx := 0; departIdx < countDeparts; departIdx++ {
-		var countWorkers int
-		if _, err := fmt.Scan(&countWorkers); err != nil || countWorkers < 1 {
-			fmt.Println(errInput.Error())
-			return
-		}
-
-		prefs := make([]string, 0, countWorkers)
-
-		for workerIdx := 0; workerIdx < countWorkers; workerIdx++ {
-			var sign string
-			var preferredTemp int
-			if _, err := fmt.Scan(&sign, &preferredTemp); err != nil {
-				fmt.Println(errInput.Error())
-				return
-			}
-
-			temp := sign + " " + strconv.Itoa(preferredTemp)
-			prefs = append(prefs, temp)
-		}
-		changeTemperature(prefs)
 	}
 }
