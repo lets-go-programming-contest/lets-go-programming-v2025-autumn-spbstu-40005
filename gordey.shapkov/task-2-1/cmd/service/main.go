aa99348f3c0e50f5f5fd93f45bb503f5db15192e
TheEmfield <emfield337@gmail.com>
2025-09-22 16:30:29 +0300

    [TASK-2-1] linter

--- gordey.shapkov/task-2-1/cmd/service/main.go
+++ gordey.shapkov/task-2-1/cmd/service/main.go
@@ -56,7 +56,8 @@ func changeTemperature(preferences []string) {
 
 		if preferedTemp < MinTemp || preferedTemp > MaxTemp {
 			currTemp = -1
-		} else if idx == 0 {
+		}
+		if idx == 0 {
 			currTemp = MinTemp
 			if sign == ">=" {
 				currTemp = preferedTemp
