3d7a8b01c197b72216d85cd1e88c77667a50c322
TvoyBatyA12343 <maxdem06@gmail.com>
2025-09-23 17:33:31 +0300

    [TASK-2-1] base program structure implemented

--- maxim.demehin/task-2-1/cmd/service/main.go
+++ maxim.demehin/task-2-1/cmd/service/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+)
+
+const (
+	lowerLimit = 15
+	upperLimit = 30
+)
+
+func selectOptimalTemperature(workersCnt int) (int, error) {
+
+}
+
+func main() {
+	var departsCount int
+
+	_, err := fmt.Scan(&departsCount)
+	if err != nil {
+		fmt.Println("Invalid count of departments")
+		return
+	}
+
+	var (
+		workersCount int
+		temperature  int
+		cmpSign      string
+	)
+
+	for departsInd := 0; departsInd < departsCount; departsInd++ {
+		_, err = fmt.Scan(&workersCount)
+		if err != nil {
+			fmt.Println("Invalid count of workers")
+			return
+		}
+		for workersInd := 0; workersInd < workersCount; workersInd++ {
+
+		}
+
+	}
+
+}
