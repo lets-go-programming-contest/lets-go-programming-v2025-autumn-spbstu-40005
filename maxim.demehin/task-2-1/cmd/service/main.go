708193aa2dfb283e7271045285281a6a616f9e85
TvoyBatyA12343 <maxdem06@gmail.com>
2025-09-24 17:20:56 +0300

    [TASK-2-1] fix for linter

--- maxim.demehin/task-2-1/cmd/service/main.go
+++ maxim.demehin/task-2-1/cmd/service/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 var (
-	errCmpInput   = errors.New("Unknown comparator")
-	errInput      = errors.New("Input error")
-	errOutOfRange = errors.New("Temperature is out of range")
+	errCmpInput   = errors.New("unknown comparator")
+	errInput      = errors.New("input error")
+	errOutOfRange = errors.New("temperature is out of range")
 )
 
 const (
@@ -17,7 +17,6 @@ const (
 )
 
 func handleOptimalTemperature(cmp string, curr int, lower, upper *int) error {
-	// Если диапазон уже пуст, всегда возвращаем -1
 	if *lower > *upper {
 		fmt.Println(-1)
 		return nil
@@ -27,30 +26,27 @@ func handleOptimalTemperature(cmp string, curr int, lower, upper *int) error {
 	case ">=":
 		switch {
 		case curr >= *lower && curr <= *upper:
-			*lower = curr // Поднимаем нижнюю границу
+			*lower = curr
 			fmt.Println(*lower)
 		case curr > *upper:
-			// Невозможное условие - сужаем диапазон до пустого
 			*lower = *upper + 1
 			fmt.Println(-1)
-		default: // curr < *lower
-			// Условие уже выполняется, оставляем как есть
+		default:
 			fmt.Println(*lower)
 		}
 	case "<=":
 		switch {
 		case curr <= *upper && curr >= *lower:
-			*upper = curr // Опускаем верхнюю границу
+			*upper = curr
 			fmt.Println(*lower)
 		case curr < *lower:
-			// Невозможное условие - сужаем диапазон до пустого
 			*upper = *lower - 1
 			fmt.Println(-1)
-		default: // curr > *upper
-			// Условие уже выполняется, оставляем как есть
+		default:
 			fmt.Println(*lower)
 		}
 	default:
+
 		return errCmpInput
 	}
 
@@ -68,17 +64,22 @@ func handleDepartmentTemperatures(workersCnt int) error {
 	for range workersCnt {
 		_, err := fmt.Scan(&cmpSign, &temperature)
 		if err != nil {
+
 			return errInput
 		}
+
 		if temperature > upperLimit || temperature < lowerLimit {
+
 			return errOutOfRange
 		}
 
 		err = handleOptimalTemperature(cmpSign, temperature, &currLower, &currUpper)
 		if err != nil {
+
 			return err
 		}
 	}
+
 	return nil
 }
 
@@ -91,6 +92,7 @@ func main() {
 	_, err := fmt.Scan(&departsCount)
 	if err != nil {
 		fmt.Println(errInput.Error())
+
 		return
 	}
 
@@ -98,12 +100,14 @@ func main() {
 		_, err = fmt.Scan(&workersCount)
 		if err != nil {
 			fmt.Println(errInput.Error())
+
 			return
 		}
 
 		err = handleDepartmentTemperatures(workersCount)
 		if err != nil {
 			fmt.Println(err.Error())
+
 			return
 		}
 	}
