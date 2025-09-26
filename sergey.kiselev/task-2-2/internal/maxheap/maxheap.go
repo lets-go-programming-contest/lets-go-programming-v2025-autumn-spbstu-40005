87e3fa17f764174299a430834badbed396692a14
deonik3 <kis.sergey2006@gmail.com>
2025-09-26 20:51:40 +0300

    [TASK-2-2] the heap interface has been made

---
+++ sergey.kiselev/task-2-2/internal/maxheap/maxheap.go
@@ -0,0 +1,29 @@
+type MaxHeap []int
+
+func (h MaxHeap) Len() int {
+	return len(h)
+}
+
+func (h MaxHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h MaxHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MaxHeap) Push(x any) {
+	if val, ok := x.(int); ok {
+		*h = append(*h, val)
+	} else {
+		panic("value is not an int")
+	}
+}
+
+func (h *MaxHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
