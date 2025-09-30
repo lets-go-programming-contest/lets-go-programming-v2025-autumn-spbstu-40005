a6862b57495a93fb03c7eb76a4ecc53c4be87f38
TvoyBatyA12343 <maxdem06@gmail.com>
2025-09-30 15:20:58 +0300

    [TASK-2-2] lint

--- maxim.demehin/task-2-2/internal/intheap/intheap.go
+++ maxim.demehin/task-2-2/internal/intheap/intheap.go
@@ -24,19 +24,20 @@ func (h *IntHeap) Pop() any {
 	origLength := len(orig)
 	toreturn := orig[origLength-1]
 	*h = orig[0 : origLength-1]
+
 	return toreturn
 }
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) GetNth(numberOfElement int) int {
