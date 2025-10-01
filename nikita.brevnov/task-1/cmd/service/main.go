261240407e2f1ba260cccc65b5a13d58f34b3c03
Brev1ch <flirktv@gmail.com>
2025-10-02 02:56:09 +0300

    [TASK-1] Calculator

--- nikita.brevnov/task-1/cmd/service/main.go
+++ nikita.brevnov/task-1/cmd/service/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var oper1 int
+	var oper2 int
+	var operation string
+	_, err := fmt.Scan(&oper1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&oper2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	switch operation {
+	case "+":
+		fmt.Println(oper1 + oper2)
+	case "-":
+		fmt.Println(oper1 - oper2)
+	case "*":
+		fmt.Println(oper1 * oper2)
+	case "/":
+		if oper2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(oper1 / oper2)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
