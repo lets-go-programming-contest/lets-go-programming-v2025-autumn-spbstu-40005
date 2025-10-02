e5504fc9a11bb851817f34ab7477053692fcc0f8
Rassoha <serdribas@gmail.com>
2025-10-03 01:04:05 +0300

    [TASK-2-1] initital commit

---
+++ sergey.dribas/task-2-1/cmd/service/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func processDepartment() bool {
+	var k int
+	_, err := fmt.Scan(&k)
+	if err != nil {
+		return false
+	}
+	minTemp := 15
+	maxTemp := 30
+	for i := 0; i < k; i++ {
+		var operand string
+		var temp int
+		_, err := fmt.Scan(&operand, &temp)
+		if err != nil {
+			return false
+		}
+		switch operand {
+		case ">=":
+			if temp > minTemp {
+				minTemp = temp
+			}
+		case "<=":
+			if temp < maxTemp {
+				maxTemp = temp
+			}
+		default:
+
+			return false
+		}
+		if minTemp > maxTemp {
+			fmt.Println("-1")
+		} else {
+			fmt.Println(minTemp)
+		}
+	}
+	return true
+}
+
+func main() {
+	var n int
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		return
+	}
+	for i := 0; i < n; i++ {
+		if !processDepartment() {
+			return
+		}
+	}
+}
