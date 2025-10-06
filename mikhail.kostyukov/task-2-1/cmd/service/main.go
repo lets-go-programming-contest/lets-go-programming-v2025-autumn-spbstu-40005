fdb25d224b02e6000dc1abef3b2f8e6758be42e1
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-10-06 22:38:37 +0300

    [TASK-2-1] Added base logic

--- mikhail.kostyukov/task-2-1/cmd/service/main.go
+++ mikhail.kostyukov/task-2-1/cmd/service/main.go
@@ -5,5 +5,36 @@ import (
 )
 
 func main() {
-	fmt.Println("Init")
+	var n int
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		var k int
+		fmt.Scan(&k)
+
+		minTemp := 15
+		maxTemp := 30
+
+		for j := 0; j < k; j++ {
+			var operator string
+			var temp int
+			fmt.Scan(&operator, &temp)
+
+			if operator == ">=" {
+				if temp > minTemp {
+					minTemp = temp
+				}
+			} else if operator == "<=" {
+				if temp < maxTemp {
+					maxTemp = temp
+				}
+			}
+
+			if minTemp > maxTemp {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minTemp)
+			}
+		}
+	}
 }
