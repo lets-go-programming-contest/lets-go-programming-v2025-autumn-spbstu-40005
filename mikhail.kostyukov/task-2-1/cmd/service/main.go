9e976e6f17a199c706d670898af73faaaf30e7f2
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-10-06 23:30:50 +0300

    [TASK-2-1] Added logic with structure

--- mikhail.kostyukov/task-2-1/cmd/service/main.go
+++ mikhail.kostyukov/task-2-1/cmd/service/main.go
@@ -41,6 +41,53 @@ func (tm *TemperatureManager) Update(operator string, temp int) error {
 	return nil
 }
 
-func main() {
+func (tm *TemperatureManager) GetOptimalTemp() int {
+	if tm.minTemp > tm.maxTemp {
+		return -1
+	}
+	return tm.minTemp
 }
 
+func processDepartment () error {
+	var workers int
+	if _, err := fmt.Scan(&workers); err != nil {
+		return fmt.Errorf("error when reading number of workers: %w", err)
+	}
+
+	manager := NewTemperatureManager()
+
+	for i := 0; i < workers; i++ {
+		var (
+			operator string
+			temp int
+		)
+		if _, err := fmt.Scan(&operator, &temp); err != nil {
+			return fmt.Errorf("error when reading worker input: %w", err)
+		}
+
+		if err := manager.Update(operator, temp); err != nil {
+			fmt.Printf("error when updating temperature manager: %v\n", err)
+			continue
+		}
+
+		result := manager.GetOptimalTemp()
+		fmt.Println(result)
+	}
+
+	return nil
+}
+
+func main() {
+	var departments int
+	if _, err := fmt.Scan(&departments); err != nil {
+		fmt.Printf("error when reading number of departments: %v\n", err)
+		return
+	}
+
+	for i := 0; i < departments; i++ {
+		if err := processDepartment(); err != nil {
+			fmt.Printf("error when processing department: %v\n", err)
+			return
+		}
+	}
+}
