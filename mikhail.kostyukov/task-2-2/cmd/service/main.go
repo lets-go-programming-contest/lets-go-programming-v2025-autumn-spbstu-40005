3f8e2348722b18944c1f3ba0d1cbfccd6398d2bb
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-10-07 22:58:12 +0300

    [TASK-2-2] Added main logic

--- mikhail.kostyukov/task-2-2/cmd/service/main.go
+++ mikhail.kostyukov/task-2-2/cmd/service/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
+
+	"github.com/KostyukovMichael/lets-go-programming-v2025-autumn-spbstu-40005/task-2-2/internal/intheap"
 )
 
 func main() {
-	fmt.Println("Init commit")
+	var dishesCnt int
+	if _, err := fmt.Scan(&dishesCnt); err != nil {
+		fmt.Printf("invalid input of number of dishes: %v\n", err)
+
+		return
+	}
+
+	dishesHeap := &intheap.IntHeap{}
+	heap.Init(dishesHeap)
+
+	for range dishesCnt {
+		var preference int
+		if _, err := fmt.Scan(&preference); err != nil {
+			fmt.Printf("invalid input of preference: %v\n", err)
+
+			return
+		}
+
+		heap.Push(dishesHeap, preference)
+	}
+
+	var wished int
+	if _, err := fmt.Scan(&wished); err != nil {
+		fmt.Printf("invalid input of wished dish: %v\n", err)
+
+		return
+	}
+
+	if wished < 1 || wished > dishesHeap.Len() {
+		fmt.Println("that dish does not exist")
+
+		return
+	}
+
+	var result int
+	for range wished {
+		result = heap.Pop(dishesHeap).(int)
+	}
+
+	fmt.Println(result)
 }
