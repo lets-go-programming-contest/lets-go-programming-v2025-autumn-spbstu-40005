f9e71fa733e4f4c23a34e90f247869c6a41607b8
Rassoha <serdribas@gmail.com>
2025-10-07 15:03:59 +0300

    [TASK-1] move operator read to all

--- sergey.dribas/task-1/cmd/service/main.go
+++ sergey.dribas/task-1/cmd/service/main.go
@@ -9,18 +9,16 @@ func main() {
 		leftOperand, rightOperand int
 		operation                 string
 	)
-	n, err := fmt.Scan(&leftOperand, &rightOperand)
+	n, err := fmt.Scan(&leftOperand, &rightOperand, &operation)
 	if err != nil {
-		if n == 0 {
+		switch n {
+		case 0:
 			fmt.Println("Invalid first operand")
-		} else if n == 1 {
+		case 1:
 			fmt.Println("Invalid second operand")
+		case 2:
+			fmt.Println("Invalid operation")
 		}
-		return
-	}
-	_, err = fmt.Scan(&operation)
-	if err != nil {
-		fmt.Println("Invalid operation")
 		return
 	}
 	switch operation {
