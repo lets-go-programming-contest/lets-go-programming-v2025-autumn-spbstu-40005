d5949d7592eedf8e17d4623e085c0bf850c2d2da
Robotophile <ksyunya.iv@gmail.com>
2025-10-09 23:41:46 +0300

    [TASK-1] fix lint 3

--- ksenia.ivanova/task-1/cmd/service/main.go
+++ ksenia.ivanova/task-1/cmd/service/main.go
@@ -7,25 +7,21 @@ func main() {
 		firstOperand, secondOperand int
 		operation                   string
 	)
-	
 	_, err := fmt.Scan(&firstOperand)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
-	
 	_, err = fmt.Scan(&secondOperand)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-	
 	_, err = fmt.Scan(&operation)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
-	
 	switch operation {
 	case "+":
 		fmt.Println(firstOperand + secondOperand)
