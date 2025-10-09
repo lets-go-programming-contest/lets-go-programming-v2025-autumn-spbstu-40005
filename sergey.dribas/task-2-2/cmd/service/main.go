569393ff4bb31cf7c82c74d313bee50c76f79813
Rassoha <serdribas@gmail.com>
2025-10-09 21:50:42 +0300

    [TASK-2-2] add main function

---
+++ sergey.dribas/task-2-2/cmd/service/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"sergey.dribas/task-2-2/internal/intheap"
+)
+
+func main() {
+	var (
+		size, number, predict int
+		dish                  = intheap.IntHeap{}
+	)
+
+	if _, err := fmt.Scan(&size); err != nil {
+		return
+	}
+
+	for range size {
+		if _, err := fmt.Scan(&number); err != nil {
+			return
+		}
+
+		dish.Push(number)
+	}
+
+	if _, err := fmt.Scan(&predict); err != nil {
+		return
+	}
+	fmt.Println(intheap.FindKthSmallest(dish, predict))
+}
