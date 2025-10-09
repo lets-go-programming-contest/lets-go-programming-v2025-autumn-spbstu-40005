06035a867148d1e68e47a52d1803b34f8b03863f
Savishna <bravissimo28@mail.ru>
2025-10-09 14:57:35 +0300

    [TASK-2-1] remake

--- eugene.averenkov/task-2-1/cmd/service/main.go
+++ eugene.averenkov/task-2-1/cmd/service/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -11,11 +10,7 @@ import (
 const (
 	InitialMinTemp = 15
 	InitialMaxTemp = 30
-	minPartsLength = 2
-	decimalBase    = 10
 )
-
-var ErrUnexpectedEndOfInput = errors.New("unexpected end of input")
 
 type TemperatureManager struct {
 	minTemp int
@@ -43,82 +38,50 @@ func (tm *TemperatureManager) Update(operator string, temp int) {
 }
 
 func (tm *TemperatureManager) GetOptimalTemp() int {
-	if !tm.IsPossible() {
+	if tm.minTemp > tm.maxTemp {
 		return -1
 	}
 
 	return tm.minTemp
 }
 
-func (tm *TemperatureManager) IsPossible() bool {
-	return tm.minTemp <= tm.maxTemp
-}
-
-func parseInput(scanner *bufio.Scanner) (int, error) {
-	if !scanner.Scan() {
-		return 0, fmt.Errorf("%w", ErrUnexpectedEndOfInput)
-	}
-
-	return parseInt(scanner.Text()), nil
-}
-
-func parseInt(s string) int {
+func stringToInt(s string) int {
 	num := 0
-
 	for _, ch := range s {
 		if ch >= '0' && ch <= '9' {
-			num = num*decimalBase + int(ch-'0')
+			num = num*10 + int(ch-'0')
 		}
 	}
-
 	return num
-}
-
-func processDepartment(scanner *bufio.Scanner, workers int) {
-	manager := NewTemperatureManager()
-
-	for range workers {
-		if !scanner.Scan() {
-			fmt.Println(-1)
-
-			return
-		}
-
-		line := scanner.Text()
-		parts := strings.Fields(line)
-
-		if len(parts) < minPartsLength {
-			fmt.Println(-1)
-
-			continue
-		}
-
-		operator := parts[0]
-		temp := parseInt(parts[1])
-
-		manager.Update(operator, temp)
-		fmt.Println(manager.GetOptimalTemp())
-	}
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	departments, err := parseInput(scanner)
-	if err != nil {
-		fmt.Println(-1)
+	scanner.Scan()
+	departments := stringToInt(scanner.Text())
 
-		return
-	}
+	scanner.Scan()
+	workers := stringToInt(scanner.Text())
 
-	workers, err := parseInput(scanner)
-	if err != nil {
-		fmt.Println(-1)
+	for i := 0; i < departments; i++ {
+		manager := NewTemperatureManager()
 
-		return
-	}
+		for j := 0; j < workers; j++ {
+			scanner.Scan()
+			line := scanner.Text()
+			parts := strings.Fields(line)
 
-	for range departments {
-		processDepartment(scanner, workers)
+			if len(parts) < 2 {
+				fmt.Println(-1)
+				continue
+			}
+
+			operator := parts[0]
+			temp := stringToInt(parts[1])
+
+			manager.Update(operator, temp)
+			fmt.Println(manager.GetOptimalTemp())
+		}
 	}
 }
