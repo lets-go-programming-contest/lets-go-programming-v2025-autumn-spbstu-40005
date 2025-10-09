3072b3e1c4bafec6a50216b62f12cdcd0be9a75c
alina <duhaninaalina3@gmail.com>
2025-10-09 23:59:00 +0300

    [TASK-2-2] fix h, space

--- alina.duhanina/task-2-2/internal/intheap/intheap.go
+++ alina.duhanina/task-2-2/internal/intheap/intheap.go
@@ -2,30 +2,35 @@ package intheap
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (heapInstance IntHeap) Len() int {
+	return len(heapInstance)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+func (heapInstance IntHeap) Less(i, j int) bool {
+	return heapInstance[i] < heapInstance[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (heapInstance IntHeap) Swap(i, j int) {
+	heapInstance[i], heapInstance[j] = heapInstance[j], heapInstance[i]
 }
 
-func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+func (heapInstance *IntHeap) Push(x interface{}) {
+	num, ok := x.(int)
+	if !ok {
+		return
+	}
+	*heapInstance = append(*heapInstance, num)
 }
 
-func (h *IntHeap) Pop() interface{} {
-	if h.Len() == 0 {
+func (heapInstance *IntHeap) Pop() interface{} {
+	if heapInstance.Len() == 0 {
 		return nil
 	}
-	old := *h
+	old := *heapInstance
+
 	n := len(old)
 	x := old[n-1]
-	*h = old[0 : n-1]
+	*heapInstance = old[0 : n-1]
 
 	return x
 }
