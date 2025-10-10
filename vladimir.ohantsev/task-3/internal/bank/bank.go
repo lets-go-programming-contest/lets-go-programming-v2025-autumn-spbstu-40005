506b7e9dbac3d5deb9179be34339f9e2b56b90a0
P3rCh1 <okhantsev.vova@mail.ru>
2025-10-10 13:05:53 +0300

    [TASK-3] linter

--- vladimir.ohantsev/task-3/internal/bank/bank.go
+++ vladimir.ohantsev/task-3/internal/bank/bank.go
@@ -1,11 +1,11 @@
 package bank
 
 type Bank struct {
-	Currencies []Currency `xml:"Valute" json:"valute"`
+	Currencies []Currency ` json:"valute" xml:"Valute"`
 }
 
 type Currency struct {
-	NumCode  int       `xml:"NumCode" json:"num_code"`
-	CharCode string    `xml:"CharCode" json:"char_code"`
-	Value    valueType `xml:"Value" json:"value"`
+	NumCode  int       `json:"num_code" xml:"NumCode"`
+	CharCode string    `json:"char_code" xml:"CharCode" `
+	Value    valueType `json:"value" xml:"Value"`
 }
