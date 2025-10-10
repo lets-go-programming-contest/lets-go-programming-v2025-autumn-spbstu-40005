da2679263b00857cbc1814102e8a1cbc0ad4d1f8
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-10-10 20:50:29 +0300

    [TASK-1] small fix

--- oleg.zholobov/task-1/cmd/service/main.go
+++ oleg.zholobov/task-1/cmd/service/main.go
@@ -4,21 +4,21 @@ import "fmt"
 
 func main() {
 	var firstOperand int
-	_, err := fmt.Scan(&firstOperand)
+	_, err := fmt.Scanln(&firstOperand)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
 	var secondOperand int
-	_, err = fmt.Scan(&secondOperand)
+	_, err = fmt.Scanln(&secondOperand)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
 
 	var operation string
-	_, err = fmt.Scan(&operation)
+	_, err = fmt.Scanln(&operation)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
