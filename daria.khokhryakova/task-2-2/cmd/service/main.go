8d4f707552a49e487e1ee26b209fd0e05e161d6a
DariaKhokhryakova <18.03.2015dasha@gmail.com>
2025-10-10 19:01:07 +0300

    [TASK-2-2] initial commit

---
+++ daria.khokhryakova/task-2-2/cmd/service/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+    "container/heap"
+    "errors"
+    "fmt"
+
+    "github.com/DariaKhokhryakova/task-2-2/internal/intheap"
+)
+
+const (
+    minDishes = 1
+    maxDishes = 10000
+    minRating = -10000
+    maxRating = 10000
+)
+
+var (
+    errInput  = errors.New("invalid input")
+    errFormat = errors.New("invalid format")
+)
+
+func priorityDish(resHeap *intheap.IntHeap, num int) (int, error) {
+    if num < minDishes || num > resHeap.Len() {
+        return 0, errInput
+    }
+
+    for i := 0; i < num-1; i++ {
+        heap.Pop(resHeap)
+    }
+
+    resPop := heap.Pop(resHeap)
+    resPriority, ok := resPop.(int)
+    if !ok {
+        return 0, errFormat
+    }
+
+    return resPriority, nil
+}
+
+func main() {
+    var numberDishes int
+
+    _, err := fmt.Scan(&numberDishes)
+    if err != nil || numberDishes < minDishes || numberDishes > maxDishes {
+        fmt.Println(errInput.Error())
+        return
+    }
+
+    resHeap := &intheap.IntHeap{}
+    heap.Init(resHeap)
+
+    for i := 0; i < numberDishes; i++ {
+        var rating int
+
+        _, err := fmt.Scan(&rating)
+        if err != nil || rating > maxRating || rating < minRating {
+            fmt.Println(errInput.Error())
+            return
+
+        }
+
+        heap.Push(resHeap, rating)
+    }
+
+    var num int
+
+    _, err = fmt.Scan(&num)
+    if err != nil || num > numberDishes || num < minDishes {
+        fmt.Println(errInput.Error())
+        return
+    }
+
+    result, err := priorityDish(resHeap, num)
+    if err != nil {
+        fmt.Println(err)
+        retutn
+    }
+
+    fmt.Println(result)
+}
+
---
+++ daria.khokhryakova/task-2-2/internal/intheap/intheap.go
@@ -0,0 +1,38 @@
+package intheap
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+    return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+    return h[i] > h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+    h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x any) {
+    intNum, ok := x.(int)
+    if !ok {
+        panic("Invalid input")
+    }
+    *h = append(*h, intNum)
+}
+
+func (h *IntHeap) Pop() any {
+    if h.Len() == 0 {
+        return nil
+
+    }
+
+    oldHeap := *h
+    n := len(oldHeap)
+    res := oldHeap[n-1]
+    *h = oldHeap[0 : n-1]
+
+    return res
+}
+
