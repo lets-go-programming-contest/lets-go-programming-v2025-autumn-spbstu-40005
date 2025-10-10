9759335a5dae673fd504268dc11b71038f96c0a7
alina <duhaninaalina3@gmail.com>
2025-10-10 12:49:25 +0300

    [TASK-2-1] delete dept

--- alina.duhanina/task-2-1/cmd/service/main.go
+++ alina.duhanina/task-2-1/cmd/service/main.go
@@ -11,11 +11,6 @@ var (
 	ErrInvalidRead  = errors.New("invalid read")
 )
 
-type Department struct {
-	Employees int
-	Requests  []Request
-}
-
 type Request struct {
 	Operator string
 	Temp     int
@@ -26,7 +21,7 @@ func readInt() (int, error) {
 
 	_, err := fmt.Scan(&value)
 	if err != nil {
-		return 0, ErrInvalidInput
+		return 0, fmt.Errorf("%w: %v", ErrInvalidInput, err)
 	}
 
 	return value, nil
@@ -40,53 +35,10 @@ func readEmployeeRequest() (Request, error) {
 
 	_, err := fmt.Scan(&operator, &temp)
 	if err != nil {
-		return Request{}, ErrInvalidInput
+		return Request{}, fmt.Errorf("%w: %v", ErrInvalidInput, err)
 	}
 
 	return Request{Operator: operator, Temp: temp}, nil
-}
-
-func readDepartment() (Department, error) {
-	employeeCount, err := readInt()
-	if err != nil {
-		return Department{}, err
-	}
-
-	dept := Department{
-		Employees: employeeCount,
-		Requests:  []Request{},
-	}
-
-	for range employeeCount {
-		req, err := readEmployeeRequest()
-		if err != nil {
-			return Department{}, ErrInvalidRead
-		}
-
-		dept.Requests = append(dept.Requests, req)
-	}
-
-	return dept, nil
-}
-
-func readInput() ([]Department, error) {
-	departmentCount, err := readInt()
-	if err != nil {
-		return nil, err
-	}
-
-	departments := make([]Department, 0, departmentCount)
-
-	for range departmentCount {
-		dept, err := readDepartment()
-		if err != nil {
-			return nil, ErrInvalidRead
-		}
-
-		departments = append(departments, dept)
-	}
-
-	return departments, nil
 }
 
 func updateTemperatureRange(minTemp, maxTemp int, req Request) (int, int) {
@@ -112,27 +64,47 @@ func getTemperatureResult(minTemp, maxTemp int) string {
 	return "-1"
 }
 
-func processDepartmentRequests(dept Department) []string {
-	results := make([]string, 0, len(dept.Requests))
+func processDepartmentRequests(employeeCount int) ([]string, error) {
+	results := make([]string, 0, employeeCount)
 	minTemp, maxTemp := 15, 30
 
-	for _, req := range dept.Requests {
+	for range employeeCount {
+		req, err := readEmployeeRequest()
+		if err != nil {
+			return nil, fmt.Errorf("%w: %v", ErrInvalidRead, err)
+		}
+
 		minTemp, maxTemp = updateTemperatureRange(minTemp, maxTemp, req)
+
 		results = append(results, getTemperatureResult(minTemp, maxTemp))
 	}
 
-	return results
+	return results, nil
 }
 
-func collectAllResults(departments []Department) []string {
+func readAndProcessInput() ([]string, error) {
+        departmentCount, err := readInt()
+        if err != nil {
+                return nil, err
+        }
+
 	allResults := make([]string, 0)
 
-	for _, dept := range departments {
-		deptResults := processDepartmentRequests(dept)
-		allResults = append(allResults, deptResults...)
-	}
+        for range departmentCount {
+                employeeCount, err := readInt()
+                if err != nil {
+                        return nil, err
+                }
 
-	return allResults
+                deptResults, err := processDepartmentRequests(employeeCount)
+                if err != nil {
+                        return nil, fmt.Errorf("%w: %v", ErrInvalidRead, err)
+                }
+
+                allResults = append(allResults, deptResults...)
+        }
+
+        return allResults, nil
 }
 
 func printResults(results []string) {
@@ -142,13 +114,12 @@ func printResults(results []string) {
 }
 
 func main() {
-	departments, err := readInput()
+	allResults, err := readAndProcessInput()
 	if err != nil {
-		fmt.Printf("Invalid read")
+		fmt.Printf("Invalid read: %v", err)
 
 		return
 	}
 
-	results := collectAllResults(departments)
-	printResults(results)
+	printResults(allResults)
 }
