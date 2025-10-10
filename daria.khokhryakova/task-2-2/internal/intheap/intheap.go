eebfa4aa9f91c6d00cff47df1b35b2f3bba6f58b
DariaKhokhryakova <18.03.2015dasha@gmail.com>
2025-10-10 19:07:04 +0300

    [TASK-2-2] fix build

--- daria.khokhryakova/task-2-2/internal/intheap/intheap.go
+++ daria.khokhryakova/task-2-2/internal/intheap/intheap.go
@@ -3,36 +3,35 @@ package intheap
 type IntHeap []int
 
 func (h IntHeap) Len() int {
-    return len(h)
+	return len(h)
 }
 
 func (h IntHeap) Less(i, j int) bool {
-    return h[i] > h[j]
+	return h[i] > h[j]
 }
 
 func (h IntHeap) Swap(i, j int) {
-    h[i], h[j] = h[j], h[i]
+	h[i], h[j] = h[j], h[i]
 }
 
 func (h *IntHeap) Push(x any) {
-    intNum, ok := x.(int)
-    if !ok {
-        panic("Invalid input")
-    }
-    *h = append(*h, intNum)
+	intNum, ok := x.(int)
+	if !ok {
+		panic("Invalid input")
+	}
+	*h = append(*h, intNum)
 }
 
 func (h *IntHeap) Pop() any {
-    if h.Len() == 0 {
-        return nil
+	if h.Len() == 0 {
+		return nil
 
-    }
+	}
 
-    oldHeap := *h
-    n := len(oldHeap)
-    res := oldHeap[n-1]
-    *h = oldHeap[0 : n-1]
+	oldHeap := *h
+	n := len(oldHeap)
+	res := oldHeap[n-1]
+	*h = oldHeap[0 : n-1]
 
-    return res
+	return res
 }
-
