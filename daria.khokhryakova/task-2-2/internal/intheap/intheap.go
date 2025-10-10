8ac768cb71ac77fbbbce28b32602e5a7d0f1863c
DariaKhokhryakova <18.03.2015dasha@gmail.com>
2025-10-10 19:29:06 +0300

    [TASK-2-2] fix lint

--- daria.khokhryakova/task-2-2/internal/intheap/intheap.go
+++ daria.khokhryakova/task-2-2/internal/intheap/intheap.go
@@ -2,30 +2,30 @@ package intheap
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x any) {
 	intNum, ok := x.(int)
 	if !ok {
-		panic("Invalid input")
+		panic("invalid input: expected int type")
 	}
+
 	*h = append(*h, intNum)
 }
 
 func (h *IntHeap) Pop() any {
 	if h.Len() == 0 {
 		return nil
-
 	}
 
 	oldHeap := *h
