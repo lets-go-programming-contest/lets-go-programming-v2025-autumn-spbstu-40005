fc55cf570de19e2dc07e77241fae8c9ff35f7a34
Savishna <bravissimo28@mail.ru>
2025-10-10 11:30:36 +0300

    [TASK-2-1] remake

--- eugene.averenkov/task-2-1/cmd/service/main.go
+++ eugene.averenkov/task-2-1/cmd/service/main.go
@@ -14,79 +14,58 @@ const (
 	minPartsLength = 2
 )
 
-type TemperatureManager struct {
-	minTemp int
-	maxTemp int
-}
-
-func NewTemperatureManager() *TemperatureManager {
-	return &TemperatureManager{
-		minTemp: InitialMinTemp,
-		maxTemp: InitialMaxTemp,
-	}
-}
-
-func (tm *TemperatureManager) Update(operator string, temp int) {
-	switch operator {
-	case ">=":
-		if temp > tm.minTemp {
-			tm.minTemp = temp
-		}
-	case "<=":
-		if temp < tm.maxTemp {
-			tm.maxTemp = temp
-		}
-	}
-}
-
-func (tm *TemperatureManager) GetOptimalTemp() int {
-	if tm.minTemp > tm.maxTemp {
-		return -1
-	}
-
-	return tm.minTemp
-}
-
-func stringToInt(s string) int {
-	num := 0
-
-	for _, ch := range s {
-		if ch >= '0' && ch <= '9' {
-			num = num*decimalBase + int(ch-'0')
-		}
-	}
-
-	return num
-}
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	scanner.Scan()
-	departments := stringToInt(scanner.Text())
+	departments := parseInt(scanner.Text())
 
-	scanner.Scan()
-	workers := stringToInt(scanner.Text())
+	for range make([]struct{}, departments) {
+		scanner.Scan()
+		workers := parseInt(scanner.Text())
 
-	for range departments {
-		manager := NewTemperatureManager()
+		minTemp := InitialMinTemp
+		maxTemp := InitialMaxTemp
 
-		for range workers {
+		for range make([]struct{}, workers) {
 			scanner.Scan()
 			line := scanner.Text()
 			parts := strings.Fields(line)
 
 			if len(parts) < minPartsLength {
 				fmt.Println(-1)
-
 				continue
 			}
 
 			operator := parts[0]
-			temp := stringToInt(parts[1])
+			temp := parseInt(parts[1])
 
-			manager.Update(operator, temp)
-			fmt.Println(manager.GetOptimalTemp())
+			switch operator {
+			case ">=":
+				if temp > minTemp {
+					minTemp = temp
+				}
+			case "<=":
+				if temp < maxTemp {
+					maxTemp = temp
+				}
+			}
+
+			if minTemp > maxTemp {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minTemp)
+			}
 		}
 	}
+}
+
+func parseInt(s string) int {
+	num := 0
+	for _, ch := range []byte(s) {
+		if ch >= '0' && ch <= '9' {
+			num = num*decimalBase + int(ch-'0')
+		}
+	}
+	return num
 }
