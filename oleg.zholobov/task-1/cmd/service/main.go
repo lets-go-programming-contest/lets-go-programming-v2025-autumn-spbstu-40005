363ac054293a7708ee94cae34e76079c5f49825e
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-10-10 20:31:36 +0300

    [TASK-1] Initial commit

---
+++ oleg.zholobov/task-1/cmd/service/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstOperand int
+	fmt.Scan(&firstOperand)
+
+	var secondOperand int
+	fmt.Scan(&secondOperand)
+
+	var operation string
+	fmt.Scan(&operation)
+
+	fmt.Println(firstOperand)
+	fmt.Println(secondOperand)
+	fmt.Println(operation)
+
+}
