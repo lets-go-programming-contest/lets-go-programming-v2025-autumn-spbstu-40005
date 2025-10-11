f3e23a86aaf14b0ae04a66105cabf7d946485db3
deonik3 <kis.sergey2006@gmail.com>
2025-10-11 15:19:41 +0300

    [TASK-3] add parse config

--- sergey.kiselev/task-3/internal/config/config.go
+++ sergey.kiselev/task-3/internal/config/config.go
@@ -1,6 +1,33 @@
 package config
 
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Config struct {
 	InputFile  string `yaml:"input-file"`
 	OutputFile string `yaml:"output-file"`
+}
+
+func ParseFile(configPath string) (*Config, error) {
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("error opening config file: %w", err)
+	}
+	defer file.Close()
+	return ParseYaml(file)
+}
+
+func ParseYaml(reader io.Reader) (*Config, error) {
+	config := new(Config)
+	decoder := yaml.NewDecoder(reader)
+	if err := decoder.Decode(config); err != nil {
+		return nil, fmt.Errorf("error decoding YAML: %w", err)
+	}
+
+	return config, nil
 }
