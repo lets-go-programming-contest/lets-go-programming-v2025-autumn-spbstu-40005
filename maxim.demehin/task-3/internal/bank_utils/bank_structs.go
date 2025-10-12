06efaafd7c33748c3733dbf635fc5c4b35637a49
TvoyBatyA12343 <maxdem06@gmail.com>
2025-10-12 12:46:11 +0300

    [TASK-3] bank structures implemented

---
+++ maxim.demehin/task-3/internal/bank_utils/bank_structs.go
@@ -0,0 +1,28 @@
+package bank
+
+import (
+	"encoding/xml"
+)
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Date    string   `xml:"Date,attr"`
+	Name    string   `xml:"name,attr"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+	ID        string `xml:"ID,attr"`
+	NumCode   int    `xml:"NumCode"`
+	CharCode  string `xml:"CharCode"`
+	Nominal   int    `xml:"Nominal"`
+	Name      string `xml:"Name"`
+	Value     string `xml:"Value"`
+	VunitRate string `xml:"VunitRate"`
+}
+
+type ValuteToOut struct {
+	NumCode  int     `json:"num_code"`
+	CharCode string  `json:"char_code"`
+	Value    float64 `json:"value"`
+}
