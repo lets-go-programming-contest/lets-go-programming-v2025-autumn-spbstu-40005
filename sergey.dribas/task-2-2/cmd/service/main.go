0fc3b27faf48abb4872038975d8ca7e652562343
Rassoha <serdribas@gmail.com>
2025-10-12 23:00:22 +0300

    [TASK-2-2] fix review changes

--- sergey.dribas/task-2-2/cmd/service/main.go
+++ sergey.dribas/task-2-2/cmd/service/main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"container/heap"
+	"errors"
 	"fmt"
 
 	"sergey.dribas/task-2-2/internal/intheap"
 )
 
+var (
+	ErrHeap = errors.New("heap size less than number")
+	ErrType = errors.New("unexpected type from heap pop")
+)
+
+func FindKthSmallest(intHeap *intheap.IntHeap, number int) (int, error) {
+	if intHeap.Len() < number {
+		return 0, ErrHeap
+	}
+
+	copyHeap := make(intheap.IntHeap, intHeap.Len())
+	copy(copyHeap, *intHeap)
+
+	for range copyHeap.Len() - number {
+		heap.Pop(&copyHeap)
+	}
+
+	result := heap.Pop(&copyHeap)
+	if val, ok := result.(int); ok {
+		return val, nil
+	}
+
+	return 0, ErrType
+}
+
 func main() {
 	var (
-		size, number, predict int
-		dish                  = &intheap.IntHeap{}
+		size int
+		dish = &intheap.IntHeap{}
 	)
+
+	heap.Init(dish)
 
 	if _, err := fmt.Scan(&size); err != nil {
 		return
 	}
 
+	var number int
 	for range size {
 		if _, err := fmt.Scan(&number); err != nil {
 			return
@@ -24,11 +54,12 @@ func main() {
 		dish.Push(number)
 	}
 
+	var predict int
 	if _, err := fmt.Scan(&predict); err != nil {
 		return
 	}
 
-	if result, err := intheap.FindKthSmallest(dish, predict); err == nil {
+	if result, err := FindKthSmallest(dish, predict); err == nil {
 		fmt.Println(result)
 	}
 }
