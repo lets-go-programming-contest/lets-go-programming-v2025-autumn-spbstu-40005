09313a418eac7bebff5e6f5cb0fb0b17d047034c
TvoyBatyA12343 <maxdem06@gmail.com>
2025-10-12 19:43:52 +0300

    [TASK-3] add conversion of string values to float + enhance XML parsing

--- maxim.demehin/task-3/internal/bank_utils/bank_structs.go
+++ maxim.demehin/task-3/internal/bank_utils/bank_structs.go
@@ -2,6 +2,9 @@ package bank
 
 import (
 	"encoding/xml"
+	"fmt"
+	"sort"
+	"strconv"
 )
 
 type ValCurs struct {
@@ -11,7 +14,14 @@ type ValCurs struct {
 	Valutes []Valute `xml:"Valute"`
 }
 
-type Valute struct {
+type ValCursRaw struct {
+	XMLName xml.Name    `xml:"ValCurs"`
+	Date    string      `xml:"Date,attr"`
+	Name    string      `xml:"name,attr"`
+	Valutes []ValuteRaw `xml:"Valute"`
+}
+
+type ValuteRaw struct {
 	ID        string `xml:"ID,attr"`
 	NumCode   int    `xml:"NumCode"`
 	CharCode  string `xml:"CharCode"`
@@ -21,8 +31,71 @@ type Valute struct {
 	VunitRate string `xml:"VunitRate"`
 }
 
+type Valute struct {
+	ID        string  `xml:"ID,attr"`
+	NumCode   int     `xml:"NumCode"`
+	CharCode  string  `xml:"CharCode"`
+	Nominal   int     `xml:"Nominal"`
+	Name      string  `xml:"Name"`
+	Value     float64 `xml:"Value"`
+	VunitRate string  `xml:"VunitRate"`
+}
+
 type ValuteToOut struct {
 	NumCode  int     `json:"num_code"`
 	CharCode string  `json:"char_code"`
 	Value    float64 `json:"value"`
+}
+
+func (v *Valute) convertFromRaw(valuteRaw ValuteRaw) error {
+	v.Nominal = valuteRaw.Nominal
+	v.Name = valuteRaw.Name
+	v.VunitRate = valuteRaw.VunitRate
+
+	floatVal, err := strconv.ParseFloat(valuteRaw.Value, 64)
+	if err != nil {
+		return fmt.Errorf("failed conversion from ValuteRaw to Valute: %w", err)
+	}
+
+	v.Value = floatVal
+
+	return nil
+}
+
+func (v *ValCurs) convertFromRaw(valCursRaw ValCursRaw) error {
+	temp := ValCurs{
+		XMLName: valCursRaw.XMLName,
+		Date:    valCursRaw.Date,
+		Name:    valCursRaw.Name,
+	}
+
+	temp.Valutes = make([]Valute, len(valCursRaw.Valutes))
+
+	for index := range valCursRaw.Valutes {
+		err := temp.Valutes[index].convertFromRaw(valCursRaw.Valutes[index])
+		if err != nil {
+			return fmt.Errorf("failed conversion from ValCursRaw to ValCurs: %w", err)
+		}
+	}
+
+	*v = temp
+	return nil
+}
+
+type ByValue []Valute
+
+func (a ByValue) Len() int {
+	return len(a)
+}
+
+func (a ByValue) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a ByValue) Less(i, j int) bool {
+	return a[i].Value > a[j].Value
+}
+
+func (v *ValCurs) sortValutes() {
+	sort.Sort(ByValue(v.Valutes))
 }
