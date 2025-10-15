3cc540ac59a91277ec53fbb531bf203898affee0
hahaha_polina <polina.gavrilova2022@mail.ru>
2025-10-15 20:27:51 +0300

    [TASK-2-2] The main structure for main.go, input processing

--- polina.gavrilova/task-2-2/cmd/main.go
+++ polina.gavrilova/task-2-2/cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var nDishes int
+
+	_, err := fmt.Scan(&nDishes)
+	if err != nil || nDishes <= 0 {
+		fmt.Printf("Invalid number of dishes: %v\n", err)
+
+		return
+	}
+
+	ratings := make([]int, nDishes)
+	for i := range nDishes {
+		_, err := fmt.Scan(&ratings[i])
+		if err != nil {
+			fmt.Printf("Invalid rating: %v\n", err)
+			return
+		}
+	}
+
+	var preferenceOrder int
+	_, err = fmt.Scan(&preferenceOrder)
+	if err != nil || preferenceOrder <= 0 || preferenceOrder > nDishes {
+		fmt.Printf("Invalid preference order: %v\n", err)
+		return
+	}
+
+	result := 0
+	fmt.Println(result)
 }
