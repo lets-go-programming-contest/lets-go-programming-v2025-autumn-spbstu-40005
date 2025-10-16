9dd20ceb1601c55fda6013327b167c4800b83737
0Revaivel0 <kharlamov_k1999@bk.ru>
2025-10-16 21:00:20 +0300

    [TASK-2-2] main

---
+++ kirill.kharlamov/task-2-2/cmd/service/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"kirill.kharlamov/task-2-2/internal/intheap"
+)
+
+func main() {
+	var totalItems int
+
+	_, err := fmt.Scan(&totalItems)
+	if err != nil || totalItems <= 0 {
+		fmt.Printf("Invalid number of dishes: %v\n", err)
+		return
+	}
+
+	scores := make([]int, totalItems)
+	for i := 0; i < totalItems; i++ {
+		_, err := fmt.Scan(&scores[i])
+		if err != nil {
+			fmt.Printf("Invalid rating: %v\n", err)
+			return
+		}
+	}
+
+	var selectionIndex int
+	_, err = fmt.Scan(&selectionIndex)
+	if err != nil || selectionIndex <= 0 || selectionIndex > totalItems {
+		fmt.Printf("Invalid preference order: %v\n", err)
+		return
+	}
+
+	finalChoice := intheap.FindKthPreference(scores, selectionIndex)
+	fmt.Println(finalChoice)
+}
