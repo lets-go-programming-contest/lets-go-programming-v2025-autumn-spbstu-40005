de16a4cdd515f147e781551adcab6891432e4f5b
Brev1ch <flirktv@gmail.com>
2025-10-17 02:57:11 +0300

    [TASK-2-1] Structures

--- nikita.brevnov/task-2-1/cmd/service/main.go
+++ nikita.brevnov/task-2-1/cmd/service/main.go
@@ -1,3 +1,24 @@
 package main
 
+import "errors"
+
+var ErrOperator = errors.New("invalid operator")
+
+const (
+	MinTemp = 15
+	MaxTemp = 30
+)
+
+type climate struct {
+	lowTemp  int
+	HighTemp int
+}
+
+func climateController() *climate {
+	return &climate{
+		lowTemp:  MinTemp,
+		HighTemp: MaxTemp,
+	}
+}
+
 func main() {}
