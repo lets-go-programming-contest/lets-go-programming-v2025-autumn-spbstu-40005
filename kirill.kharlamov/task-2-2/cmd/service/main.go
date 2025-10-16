c3b4b72a5a670641207b97227c627c83130db6e7
0Revaivel0 <kharlamov_k1999@bk.ru>
2025-10-16 21:09:56 +0300

    [TASK-2-2] fix main lint

--- kirill.kharlamov/task-2-2/cmd/service/main.go
+++ kirill.kharlamov/task-2-2/cmd/service/main.go
@@ -12,6 +12,7 @@ func main() {
 	_, err := fmt.Scan(&totalItems)
 	if err != nil || totalItems <= 0 {
 		fmt.Printf("Invalid number of dishes: %v\n", err)
+
 		return
 	}
 
@@ -20,6 +21,7 @@ func main() {
 		_, err := fmt.Scan(&scores[i])
 		if err != nil {
 			fmt.Printf("Invalid rating: %v\n", err)
+
 			return
 		}
 	}
@@ -28,6 +30,7 @@ func main() {
 	_, err = fmt.Scan(&selectionIndex)
 	if err != nil || selectionIndex <= 0 || selectionIndex > totalItems {
 		fmt.Printf("Invalid preference order: %v\n", err)
+
 		return
 	}
 
