949e3f8ff6ac24b3bacb6d6a900eeadd95db5fa5
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-10-17 19:10:41 +0300

    [TASK-2-2] added heap

--- oleg.zholobov/task-2-2/cmd/service/main.go
+++ oleg.zholobov/task-2-2/cmd/service/main.go
@@ -2,36 +2,53 @@ package main
 
 import "fmt"
 
+type MinHeap []int
+
+func (heap MinHeap) Len() int           { return len(heap) }
+func (heap MinHeap) Less(i, j int) bool { return heap[i] < heap[j] }
+func (heap MinHeap) Swap(i, j int)      { heap[i], heap[j] = heap[j], heap[i] }
+
+func (heap *MinHeap) Push(x any) {
+	*heap = append(*heap, x.(int))
+}
+
+func (heap *MinHeap) Pop() any {
+	old := *heap
+	n := len(old)
+	x := old[n-1]
+	*heap = old[0 : n-1]
+	return x
+}
+
 func main() {
-	var n int
-	if _, err := fmt.Scan(&n); err != nil {
+	var amountOfDishes int
+	if _, err := fmt.Scan(&amountOfDishes); err != nil {
 		return
 	}
 
-	if n <= 0 {
+	if amountOfDishes <= 0 {
 		return
 	}
 
-	arr := make([]int, n)
-	for i := 0; i < n; i++ {
-		if _, err := fmt.Scan(&arr[i]); err != nil {
+	dishes := make([]int, amountOfDishes)
+	for i := 0; i < amountOfDishes; i++ {
+		if _, err := fmt.Scan(&dishes[i]); err != nil {
 			return
 		}
 	}
 
-	var k int
-	if _, err := fmt.Scan(&k); err != nil {
+	var dishNumber int
+	if _, err := fmt.Scan(&dishNumber); err != nil {
 		return
 	}
 
-	if k <= 0 || k > n {
+	if dishNumber <= 0 || dishNumber > amountOfDishes {
 		return
 	}
 
-	fmt.Println(n)
-	for _, val := range arr {
+	fmt.Println(amountOfDishes)
+	for _, val := range dishes {
 		fmt.Println(val)
 	}
-	fmt.Println(k)
-
+	fmt.Println(dishNumber)
 }
