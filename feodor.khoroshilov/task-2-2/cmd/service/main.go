3430885d5173f1cb5f687ab84afbf04aceec5689
Drain-Doglazer <witchbtch64@gmail.com>
2025-10-17 23:03:49 +0300

    [TASK-2-2] fix lint(po normalinomy)

--- feodor.khoroshilov/task-2-2/cmd/service/main.go
+++ feodor.khoroshilov/task-2-2/cmd/service/main.go
@@ -17,11 +17,13 @@ func main() {
 	var totalDishesCount int
 	if _, err := fmt.Scan(&totalDishesCount); err != nil {
 		fmt.Printf("Error reading total dishes count: %v\n", err)
+
 		return
 	}
 
 	if totalDishesCount < 1 || totalDishesCount > 10000 {
 		fmt.Println("Error: number of dishes must be between 1 and 10000")
+
 		return
 	}
 
@@ -32,11 +34,13 @@ func main() {
 		var dishRating int
 		if _, err := fmt.Scan(&dishRating); err != nil {
 			fmt.Printf("Error reading dish rating: %v\n", err)
+
 			return
 		}
 
 		if dishRating < -10000 || dishRating > 10000 {
 			fmt.Println("Error: dish rating must be between -10000 and 10000")
+
 			return
 		}
 
@@ -46,15 +50,17 @@ func main() {
 	var preferredDishPosition int
 	if _, err := fmt.Scan(&preferredDishPosition); err != nil {
 		fmt.Printf("Error reading preferred dish position: %v\n", err)
+
 		return
 	}
 
 	if preferredDishPosition < 1 || preferredDishPosition > totalDishesCount {
 		fmt.Printf("Error: preferred dish position must be between 1 and %d\n", totalDishesCount)
+
 		return
 	}
 
-	for i := 0; i < preferredDishPosition-1; i++ {
+	for range preferredDishRating - 1 {
 		heap.Pop(dishRatingsHeap)
 	}
 
