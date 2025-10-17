754f8bd4907bb7d4497661e6e23230c0bbaaba6a
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-10-17 19:03:40 +0300

    [TASK-2-2] made input

--- oleg.zholobov/task-2-2/cmd/service/main.go
+++ oleg.zholobov/task-2-2/cmd/service/main.go
@@ -1,4 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
+	var n int
+	if _, err := fmt.Scan(&n); err != nil {
+		return
+	}
+
+	if n <= 0 {
+		return
+	}
+
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		if _, err := fmt.Scan(&arr[i]); err != nil {
+			return
+		}
+	}
+
+	var k int
+	if _, err := fmt.Scan(&k); err != nil {
+		return
+	}
+
+	if k <= 0 || k > n {
+		return
+	}
+
+	fmt.Println(n)
+	for _, val := range arr {
+		fmt.Println(val)
+	}
+	fmt.Println(k)
+
 }
