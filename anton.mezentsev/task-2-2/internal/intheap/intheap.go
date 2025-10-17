bacf3ca937460749f2e9a098972e97d4e9eb8a3f
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-17 21:12:57 +0300

    [TASK-2-2] fix lint1

--- anton.mezentsev/task-2-2/internal/intheap/intheap.go
+++ anton.mezentsev/task-2-2/internal/intheap/intheap.go
@@ -5,12 +5,10 @@ import "container/heap"
 type CustomHeap []int
 
 func (h CustomHeap) Len() int {
-
 	return len(h)
 }
 
 func (h CustomHeap) Less(index1, index2 int) bool {
-
 	return h[index1] < h[index2]
 }
 
@@ -21,50 +19,39 @@ func (h CustomHeap) Swap(index1, index2 int) {
 func (h *CustomHeap) Push(x interface{}) {
 	value, correnType := x.(int)
 	if !correnType {
-
 		return
 	}
-
 	*h = append(*h, value)
 }
 
 func (h *CustomHeap) Pop() interface{} {
 	oldSlice := *h
-	n := len(oldSlice)
-
-	if n == 0 {
-
+	length := len(oldSlice)
+	if length == 0 {
 		return nil
 	}
-
-	lastElement := oldSlice[n-1]
-	*h = oldSlice[0 : n-1]
-
+	lastElement := oldSlice[length-1]
+	*h = oldSlice[0 : length-1]
 	return lastElement
 }
 
-func FindKthPreference(ratings []int, k int) int {
-	if len(ratings) == 0 || k <= 0 || k > len(ratings) {
-
+func FindKthPreference(ratings []int, preferenceOrder int) int {
+	if len(ratings) == 0 || preferenceOrder <= 0 || preferenceOrder > len(ratings) {
 		return -1
 	}
 
 	heapContainer := &CustomHeap{}
 	heap.Init(heapContainer)
-
 	for _, currentRating := range ratings {
-		if heapContainer.Len() < k {
+		if heapContainer.Len() < preferenceOrder {
 			heap.Push(heapContainer, currentRating)
 		} else if currentRating > (*heapContainer)[0] {
 			heap.Pop(heapContainer)
 			heap.Push(heapContainer, currentRating)
 		}
 	}
-
 	if heapContainer.Len() == 0 {
-
 		return -1
 	}
-
 	return (*heapContainer)[0]
 }
