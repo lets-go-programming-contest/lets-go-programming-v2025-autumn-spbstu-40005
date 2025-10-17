c46b17c9119bee6d5ae85a42af46c66befdec7f6
Drain-Doglazer <witchbtch64@gmail.com>
2025-10-17 23:46:46 +0300

    [TASK-2-2] fix lint

--- feodor.khoroshilov/task-2-2/cmd/service/main.go
+++ feodor.khoroshilov/task-2-2/cmd/service/main.go
@@ -56,11 +56,11 @@ func main() {
 
 	if preferredDishPosition < 1 || preferredDishPosition > totalDishesCount {
 		fmt.Printf("Error: preferred dish position must be between 1 and %d\n", totalDishesCount)
-		
+
 		return
 	}
 
-	for i := 0; i < preferredDishPosition-1; i++ {
+	for range preferredDishRating -1{
 		heap.Pop(dishRatingsHeap)
 	}
 
