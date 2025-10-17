c49c0f2f1149ca8aa4abab2cf637ea768007774c
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-17 21:04:53 +0300

    [TASK-2-2] everyone are done

--- anton.mezentsev/task-2-2/cmd/service/main.go
+++ anton.mezentsev/task-2-2/cmd/service/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+
+	"anton.mezentsev/task-2-2/internal/intheap"
+)
+
+func main() {
+	var totalItems int
+
+	_, err := fmt.Scan(&totalItems)
+	if err != nil || totalItems <= 0 {
+		fmt.Printf("Invalid number of dishes: %v\n", err)
+
+		return
+	}
+
+	scores := make([]int, totalItems)
+	for index := range totalItems {
+		_, err := fmt.Scan(&scores[index])
+		if err != nil {
+			fmt.Printf("Invalid rating: %v\n", err)
+
+			return
+		}
+	}
+
+	var selectionIndex int
+	_, err = fmt.Scan(&selectionIndex)
+
+	if err != nil || selectionIndex <= 0 || selectionIndex > totalItems {
+		fmt.Printf("Invalid preference order: %v\n", err)
+
+		return
+	}
+
+	finalChoice := intheap.FindKthPreference(scores, selectionIndex)
+	fmt.Println(finalChoice)
+}
