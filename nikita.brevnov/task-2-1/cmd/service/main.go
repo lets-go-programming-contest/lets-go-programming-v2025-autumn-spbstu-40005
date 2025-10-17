c729ee653102aa117faf5bed6225c7a78d29e244
Brev1ch <flirktv@gmail.com>
2025-10-17 03:07:59 +0300

    [TASK-2-1] Alpha version

--- nikita.brevnov/task-2-1/cmd/service/main.go
+++ nikita.brevnov/task-2-1/cmd/service/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var ErrOperator = errors.New("invalid operator")
 
@@ -11,14 +14,74 @@ const (
 
 type climate struct {
 	lowTemp  int
-	HighTemp int
+	highTemp int
 }
 
 func climateController() *climate {
 	return &climate{
 		lowTemp:  MinTemp,
-		HighTemp: MaxTemp,
+		highTemp: MaxTemp,
 	}
 }
 
-func main() {}
+func (oc *climate) AdjustSetting(condition string, value int) error {
+	switch condition {
+	case ">=":
+		if value > oc.lowTemp {
+			oc.lowTemp = value
+		}
+	case "<=":
+		if value < oc.highTemp {
+			oc.highTemp = value
+		}
+	default:
+		return ErrOperator
+	}
+
+	return nil
+}
+
+func (oc *climate) FindComfortTemp() int {
+	if oc.lowTemp > oc.highTemp {
+		return -1
+	}
+
+	return oc.lowTemp
+}
+
+func handleDep() error {
+	var employeeCount int
+	if _, err := fmt.Scan(&employeeCount); err != nil {
+		return fmt.Errorf("error reading employee count: %w", err)
+	}
+	climateControl := climateController()
+	for range employeeCount {
+		var (
+			condition string
+			tempValue int
+		)
+		if _, err := fmt.Scan(&condition, &tempValue); err != nil {
+			return fmt.Errorf("error reading employee input: %w", err)
+		}
+		if err := climateControl.AdjustSetting(condition, tempValue); err != nil {
+			return fmt.Errorf("error adjusting climate settings: %w", err)
+		}
+		comfortTemp := climateControl.FindComfortTemp()
+		fmt.Println(comfortTemp)
+	}
+	return nil
+}
+
+func main() {
+	var depCount int
+	if _, err := fmt.Scan(&depCount); err != nil {
+		fmt.Printf("error reading department count: %v\n", err)
+		return
+	}
+	for range depCount {
+		if err := handleDep(); err != nil {
+			fmt.Printf("error processing department: %v\n", err)
+			return
+		}
+	}
+}
