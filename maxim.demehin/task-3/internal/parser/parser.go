17ac0fb76859b7bb91d9ee26eb445f0f9febacb0
TvoyBatyA12343 <maxdem06@gmail.com>
2025-10-18 18:51:33 +0300

    [TASK-3] put funcs into different packs

--- maxim.demehin/task-3/internal/parser/parser.go
+++ maxim.demehin/task-3/internal/parser/parser.go
@@ -1,21 +1,21 @@
-package bank
+package parser
 
 import (
 	"bytes"
-	"encoding/json"
 	"encoding/xml"
 	"errors"
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
+
+	"github.com/TvoyBatyA12343/task-3/internal/bank"
 
 	"golang.org/x/net/html/charset"
 )
 
 var ErrUnsupportedCharset = errors.New("unsupported charset")
 
-func ParseXML(path string) ([]Valute, error) {
+func ParseXML(path string) ([]bank.Valute, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read XML file: %w", err)
@@ -25,14 +25,14 @@ func ParseXML(path string) ([]Valute, error) {
 	decoder := xml.NewDecoder(reader)
 	decoder.CharsetReader = getCharset
 
-	var valCurs ValCurs
+	var valCurs bank.ValCurs
 
 	err = decoder.Decode(&valCurs)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse XML: %w", err)
 	}
 
-	valCurs.sortValutes()
+	valCurs.SortValutes()
 
 	return valCurs.Valutes, nil
 }
@@ -44,25 +44,4 @@ func getCharset(charsetLabel string, input io.Reader) (io.Reader, error) {
 	}
 
 	return encoding.NewDecoder().Reader(input), nil
-}
-
-func SaveValutesToFile(valutes []Valute, output string) error {
-	dir := filepath.Dir(output)
-
-	err := os.MkdirAll(dir, 0)
-	if err != nil {
-		return fmt.Errorf("failed to create directory: %w", err)
-	}
-
-	jsonData, err := json.MarshalIndent(valutes, "", "  ")
-	if err != nil {
-		return fmt.Errorf("failed marshal to JSON: %w", err)
-	}
-
-	err = os.WriteFile(output, jsonData, 0)
-	if err != nil {
-		return fmt.Errorf("failed to write JSON to file: %w", err)
-	}
-
-	return nil
 }
