ba62e6b836ac2ffa70f9f9a20f14ab89a8fd6f30
TvoyBatyA12343 <maxdem06@gmail.com>
2025-10-18 19:16:45 +0300

    [TASK-3] delete unnessecary check

--- maxim.demehin/task-3/internal/parser/parserYAML.go
+++ maxim.demehin/task-3/internal/parser/parserYAML.go
@@ -13,10 +13,6 @@ type Config struct {
 }
 
 func LoadConfig(path string) (*Config, error) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return nil, fmt.Errorf("file %s does not exist: %w", path, err)
-	}
-
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read config file: %w", err)
