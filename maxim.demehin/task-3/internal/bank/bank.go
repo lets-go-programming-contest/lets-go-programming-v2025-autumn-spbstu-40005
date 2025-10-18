9009a86811eaeea3635ab0f44c8547526f8e8d8f
TvoyBatyA12343 <maxdem06@gmail.com>
2025-10-18 14:22:39 +0300

    [TASK-3] change type of value in Valute

--- maxim.demehin/task-3/internal/bank/bank.go
+++ maxim.demehin/task-3/internal/bank/bank.go
@@ -14,9 +14,9 @@ type ValCurs struct {
 }
 
 type Valute struct {
-	NumCode  int     `xml:"NumCode" json:"num_code"`
-	CharCode string  `xml:"CharCode" json:"char_code"`
-	Value    float64 `xml:"Value" json:"value"`
+	NumCode  int         `xml:"NumCode" json:"num_code"`
+	CharCode string      `xml:"CharCode" json:"char_code"`
+	Value    CustomFloat `xml:"Value" json:"value"`
 }
 
 type CustomFloat float64
