11e02a62f60e94fb3b6a0ad120f4e72a26d057f8
TheEmfield <emfield337@gmail.com>
2025-10-19 21:27:43 +0300

    [TASK-3] linter fix

--- gordey.shapkov/task-3/internal/config/config.go
+++ gordey.shapkov/task-3/internal/config/config.go
@@ -23,7 +23,7 @@ type value float64
 type Currency struct {
 	NumCode  int       `json:"num_code"  xml:"NumCode"`
 	CharCode string    `json:"char_code" xml:"CharCode"`
-	Value    valueType `json:"value"     xml:"Value"`
+	Value    value     `json:"value"     xml:"Value"`
 }
 
 func (val *value) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
