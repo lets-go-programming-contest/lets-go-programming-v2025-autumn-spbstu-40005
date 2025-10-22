5ff61cea4e7abfd83db3d99eae1b1d52fe0c3a9c
DariaKhokhryakova <18.03.2015dasha@gmail.com>
2025-10-22 20:09:09 +0300

    [TASK-2-1] fix comments

--- daria.khokhryakova/task-2-1/internal/temperature/temperature.go
+++ daria.khokhryakova/task-2-1/internal/temperature/temperature.go
@@ -1,19 +1,8 @@
 package temperature
 
-import (
-	"errors"
-	"fmt"
-)
+import "errors"
 
-var (
-	ErrInput = errors.New("invalid input")
-	ErrIcon  = errors.New("invalid icon")
-)
-
-const (
-	minTemperature = 15
-	maxTemperature = 30
-)
+var ErrIcon = errors.New("invalid icon")
 
 type TemperatureRange struct {
 	Min int
@@ -24,38 +13,19 @@ func (temp *TemperatureRange) IsValid() bool {
 	return temp.Min <= temp.Max
 }
 
-func ReadTemperature() (string, int, error) {
-	var operator string
-
-	var temp int
-
-	_, err := fmt.Scan(&operator, &temp)
-	if err != nil {
-		return "", 0, fmt.Errorf("read temperature: %w", err)
-	}
-
-	switch operator {
-	case ">=", "<=":
-	default:
-		return "", 0, ErrIcon
-	}
-
-	if temp < minTemperature || temp > maxTemperature {
-		return "", 0, ErrInput
-	}
-
-	return operator, temp, nil
-}
-
-func UpdateTemperature(icon string, temperature int, tempRange *TemperatureRange) {
+func UpdateTemperature(icon string, temperature int, tempRange *TemperatureRange) error {
 	switch icon {
 	case ">=":
 		if temperature > tempRange.Min {
 			tempRange.Min = temperature
 		}
+		return nil
 	case "<=":
 		if temperature < tempRange.Max {
 			tempRange.Max = temperature
 		}
+		return nil
+	default:
+		return ErrIcon
 	}
 }
