84be828af7fb690f5e3fa7592623bdfdc90b4334
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-10-23 00:09:03 +0300

    [TASK-3] Added entry to json

--- mikhail.kostyukov/task-3/cmd/service/main.go
+++ mikhail.kostyukov/task-3/cmd/service/main.go
@@ -1,10 +1,12 @@
-package main;
+package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
@@ -13,8 +15,16 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+//TODO Раскидать все по файлам
+
+//TODO Сократить число структур до минимума
+
+//TODO Исправить капитализацию ошибок (что бы это ни значило)
+
+//TODO Check error defer Close
+
 type AppConfig struct {
-	InputFile string `yaml:"input-file"`
+	InputFile  string `yaml:"input-file"`
 	OutputFile string `yaml:"output-file"`
 }
 
@@ -24,12 +34,15 @@ type ValCurs struct {
 }
 
 type Valute struct {
-	//TODO delete Nominal & Name
-	NumCode  int    `xml:"NumCode" json:"num_code"`
-	CharCode string `xml:"CharCode" json:"char_code"`
-	Nominal  int    `xml:"Nominal" json:"-"`
-	Name	 string `xml:"Name" json:"-"`
-	Value    string `xml:"Value" json:"value"`
+	NumCode  int    `json:"num_code"  xml:"NumCode"`
+	CharCode string `json:"char_code" xml:"CharCode"`
+	Value    string `json:"value"     xml:"Value"`
+}
+
+type OutputValute struct {
+	NumCode  int     `json:"num_code"`
+	CharCode string  `json:"char_code"`
+	Value    float64 `json:"value"`
 }
 
 type ByValueDesc []Valute
@@ -47,14 +60,16 @@ func (v ByValueDesc) Less(i, j int) bool {
 	//TODO add checks
 	val1 := parseFloatValue(v[i].Value)
 	val2 := parseFloatValue(v[j].Value)
+
 	return val1 > val2
 }
 
 func parseFloatValue(s string) float64 {
 	s = strings.ReplaceAll(s, ",", ".")
 	val, err := strconv.ParseFloat(s, 64)
+
 	if err != nil {
-		panic(fmt.Errorf("Error parsing value %s: %w\n", s, err))
+		panic(fmt.Errorf("Error parsing value %s: %w", s, err))
 	}
 
 	return val
@@ -63,12 +78,12 @@ func parseFloatValue(s string) float64 {
 func loadConfig(path string) *AppConfig {
 	data, err := os.ReadFile(path)
 	if err != nil {
-		panic(fmt.Errorf("Error reading file %s: %w\n", path, err))
+		panic(fmt.Errorf("Error reading file %s: %w", path, err))
 	}
 
 	var config AppConfig
 	if err := yaml.Unmarshal(data, &config); err != nil {
-		panic(fmt.Errorf("Error unmarshaling YAML: %w\n", err))
+		panic(fmt.Errorf("Error unmarshaling YAML: %w", err))
 	}
 
 	return &config
@@ -82,24 +97,37 @@ func main() {
 
 	file, err := os.Open(config.InputFile)
 	if err != nil {
-		panic(fmt.Errorf("Error reading file %s: %w\n", config.InputFile, err))
+		panic(fmt.Errorf("Error reading file %s: %w", config.InputFile, err))
 	}
 
-	defer file.Close() 	//TODO Check error defer Close
+	defer file.Close()
 
 	decoder := xml.NewDecoder(file)
 	decoder.CharsetReader = charset.NewReaderLabel
 
 	var rates ValCurs
 	if err := decoder.Decode(&rates); err != nil {
-		panic(fmt.Errorf("Error unmarshaling XML: %w\n", err))
+		panic(fmt.Errorf("Error unmarshaling XML: %w", err))
 	}
 
 	sort.Sort(ByValueDesc(rates.Valutes))
 
-	fmt.Printf("Successfully read %d valutes from %s\n", len(rates.Valutes), config.InputFile)
-
+	outputData := make([]OutputValute, 0, len(rates.Valutes))
 	for _, v := range rates.Valutes {
-		fmt.Printf("%d %s %s\n", v.NumCode, v.CharCode, v.Value)
+		outputData = append(outputData, OutputValute{v.NumCode, v.CharCode, parseFloatValue(v.Value)})
+	}
+
+	jsonData, err := json.MarshalIndent(outputData, "", "\t")
+	if err != nil {
+		panic(fmt.Errorf("Error marshaling JSON: %w", err))
+	}
+
+	outputDir := filepath.Dir(config.OutputFile)
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		panic(fmt.Errorf("Error creating directory %s: %w", outputDir, err))
+	}
+
+	if err := os.WriteFile(config.OutputFile, jsonData, 0600); err != nil {
+		panic(fmt.Errorf("Error writing file %s: %w", config.OutputFile, err))
 	}
 }
