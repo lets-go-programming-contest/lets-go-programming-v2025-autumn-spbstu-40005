87e3f225ce092f0aebf04e861a31df8b5bba0270
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-10-23 23:33:40 +0300

    [TASK-3] Become more civilized

--- mikhail.kostyukov/task-3/cmd/service/main.go
+++ mikhail.kostyukov/task-3/cmd/service/main.go
@@ -1,133 +1,29 @@
 package main
 
 import (
-	"encoding/json"
-	"encoding/xml"
 	"flag"
 	"fmt"
-	"os"
-	"path/filepath"
-	"sort"
-	"strconv"
-	"strings"
 
-	"golang.org/x/net/html/charset"
-	"gopkg.in/yaml.v3"
+	"github.com/KostyukovMichael/lets-go-programming-v2025-autumn-spbstu-40005/task-3/internal/converter"
 )
 
-//TODO Раскидать все по файлам
-
-//TODO Сократить число структур до минимума
-
-//TODO Исправить капитализацию ошибок (что бы это ни значило)
-
-//TODO Check error defer Close
-
-type AppConfig struct {
-	InputFile  string `yaml:"input-file"`
-	OutputFile string `yaml:"output-file"`
-}
-
-type ValCurs struct {
-	XMLName xml.Name `xml:"ValCurs"`
-	Valutes []Valute `xml:"Valute"`
-}
-
-type Valute struct {
-	NumCode  int    `json:"num_code"  xml:"NumCode"`
-	CharCode string `json:"char_code" xml:"CharCode"`
-	Value    string `json:"value"     xml:"Value"`
-}
-
-type OutputValute struct {
-	NumCode  int     `json:"num_code"`
-	CharCode string  `json:"char_code"`
-	Value    float64 `json:"value"`
-}
-
-type ByValueDesc []Valute
-
-func (v ByValueDesc) Len() int {
-	return len(v)
-}
-
-func (v ByValueDesc) Swap(i, j int) {
-	//TODO add checks
-	v[i], v[j] = v[j], v[i]
-}
-
-func (v ByValueDesc) Less(i, j int) bool {
-	//TODO add checks
-	val1 := parseFloatValue(v[i].Value)
-	val2 := parseFloatValue(v[j].Value)
-
-	return val1 > val2
-}
-
-func parseFloatValue(s string) float64 {
-	s = strings.ReplaceAll(s, ",", ".")
-	val, err := strconv.ParseFloat(s, 64)
-
-	if err != nil {
-		panic(fmt.Errorf("Error parsing value %s: %w", s, err))
-	}
-
-	return val
-}
-
-func loadConfig(path string) *AppConfig {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		panic(fmt.Errorf("Error reading file %s: %w", path, err))
-	}
-
-	var config AppConfig
-	if err := yaml.Unmarshal(data, &config); err != nil {
-		panic(fmt.Errorf("Error unmarshaling YAML: %w", err))
-	}
-
-	return &config
-}
+var (
+	configFlagName    = "config"
+	configFlagDefault = "config.yaml"
+	configFlagUsage   = "path to config file"
+)
 
 func main() {
-	configPath := flag.String("config", "config.yaml", "path to config file")
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("panic: %v\n", err)
+		}
+	}()
+
+	configPath := flag.String(configFlagName, configFlagDefault, configFlagUsage)
 	flag.Parse()
 
-	config := loadConfig(*configPath)
-
-	file, err := os.Open(config.InputFile)
-	if err != nil {
-		panic(fmt.Errorf("Error reading file %s: %w", config.InputFile, err))
-	}
-
-	defer file.Close()
-
-	decoder := xml.NewDecoder(file)
-	decoder.CharsetReader = charset.NewReaderLabel
-
-	var rates ValCurs
-	if err := decoder.Decode(&rates); err != nil {
-		panic(fmt.Errorf("Error unmarshaling XML: %w", err))
-	}
-
-	sort.Sort(ByValueDesc(rates.Valutes))
-
-	outputData := make([]OutputValute, 0, len(rates.Valutes))
-	for _, v := range rates.Valutes {
-		outputData = append(outputData, OutputValute{v.NumCode, v.CharCode, parseFloatValue(v.Value)})
-	}
-
-	jsonData, err := json.MarshalIndent(outputData, "", "\t")
-	if err != nil {
-		panic(fmt.Errorf("Error marshaling JSON: %w", err))
-	}
-
-	outputDir := filepath.Dir(config.OutputFile)
-	if err := os.MkdirAll(outputDir, 0755); err != nil {
-		panic(fmt.Errorf("Error creating directory %s: %w", outputDir, err))
-	}
-
-	if err := os.WriteFile(config.OutputFile, jsonData, 0600); err != nil {
-		panic(fmt.Errorf("Error writing file %s: %w", config.OutputFile, err))
+	if err := converter.Run(*configPath); err != nil {
+		fmt.Printf("converter.Run: %v\n", err)
 	}
 }
