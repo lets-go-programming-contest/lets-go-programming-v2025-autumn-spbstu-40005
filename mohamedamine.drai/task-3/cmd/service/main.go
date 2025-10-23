5fae851483b8a641bee2f5be6a5b889bdeb7557e
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-10-23 03:08:03 +0200

    [TASK-3] Complete solution

--- mohamedamine.drai/task-3/cmd/service/main.go
+++ mohamedamine.drai/task-3/cmd/service/main.go
@@ -5,7 +5,6 @@ import (
 
 	"mohamedamine.drai/task-3/internal/utils"
 )
-
 func main() {
 	configPath := flag.String("config", "", "path to YAML config file")
 	flag.Parse()
@@ -25,7 +24,6 @@ func main() {
 	}
 
 	sorted := utils.SortCurrencies(data.Currencies)
-
 	if err := utils.SaveToJSON(sorted, cfg.OutputFile); err != nil {
 		panic(err)
 	}
