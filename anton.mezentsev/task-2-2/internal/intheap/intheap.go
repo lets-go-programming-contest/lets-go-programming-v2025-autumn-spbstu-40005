e697d87127896c18724c6cebe940361ec56751f0
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-24 01:17:21 +0300

    [TASK-2-2] fix of all

--- anton.mezentsev/task-2-2/internal/intheap/intheap.go
+++ anton.mezentsev/task-2-2/internal/intheap/intheap.go
@@ -1,25 +1,29 @@
 package intheap
 
-import "container/heap"
-
 type CustomHeap []int
 
 func (h *CustomHeap) Len() int {
 	return len(*h)
 }
 
-func (h *CustomHeap) Less(index1, index2 int) bool {
-	return (*h)[index1] < (*h)[index2]
+func (h CustomHeap) Less(index1, index2 int) bool {
+	if index1 < 0 || index1 >= len(h) || index2 < 0 || index2 >= len(h) {
+		return false
+	}
+	return h[index1] < h[index2]
 }
 
-func (h *CustomHeap) Swap(index1, index2 int) {
-	(*h)[index1], (*h)[index2] = (*h)[index2], (*h)[index1]
+func (h CustomHeap) Swap(index1, index2 int) {
+	if index1 < 0 || index1 >= len(h) || index2 < 0 || index2 >= len(h) {
+		return
+	}
+	h[index1], h[index2] = h[index2], h[index1]
 }
 
 func (h *CustomHeap) Push(x interface{}) {
-	value, correctType := x.(int)
-	if !correctType {
-		return
+	value, ok := x.(int)
+	if !ok {
+		panic("IntHeap.Push: value isn't int")
 	}
 
 	*h = append(*h, value)
@@ -37,28 +41,4 @@ func (h *CustomHeap) Pop() interface{} {
 	*h = oldSlice[0 : length-1]
 
 	return lastElement
-}
-
-func FindKthPreference(ratings []int, preferenceOrder int) int {
-	if len(ratings) == 0 || preferenceOrder <= 0 || preferenceOrder > len(ratings) {
-		return -1
-	}
-
-	heapContainer := &CustomHeap{}
-	heap.Init(heapContainer)
-
-	for _, currentRating := range ratings {
-		if heapContainer.Len() < preferenceOrder {
-			heap.Push(heapContainer, currentRating)
-		} else if currentRating > (*heapContainer)[0] {
-			heap.Pop(heapContainer)
-			heap.Push(heapContainer, currentRating)
-		}
-	}
-
-	if heapContainer.Len() == 0 {
-		return -1
-	}
-
-	return (*heapContainer)[0]
 }
