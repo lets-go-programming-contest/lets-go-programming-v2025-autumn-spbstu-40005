043508de01150982cd76fecc2cfa86ca27f61473
hahaha_polina <polina.gavrilova2022@mail.ru>
2025-10-24 20:09:06 +0300

    [TASK-3] Add config loading and basic structure

--- polina.gavrilova/task-3/cmd/service/main.go
+++ polina.gavrilova/task-3/cmd/service/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"flag"
+
+	"polina.gavrilova/task-3/internal/config"
+)
+
+func main() {
+	configPath := flag.String("config", "", "path to YAML config file")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("--config flag is required")
+	}
+
+	_, err := config.LoadConfig(*configPath)
+	if err != nil {
+		panic(err)
+	}
+}
