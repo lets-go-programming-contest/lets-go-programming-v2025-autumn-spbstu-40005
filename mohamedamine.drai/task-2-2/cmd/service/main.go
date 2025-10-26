7adfea96e54f389e45881dc8e1a1d4f724c0ff15
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-10-26 02:38:54 +0200

    [TASK-2-2] Fix

--- mohamedamine.drai/task-2-2/cmd/service/main.go
+++ mohamedamine.drai/task-2-2/cmd/service/main.go
@@ -14,16 +14,16 @@ var (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x interface{}) {
