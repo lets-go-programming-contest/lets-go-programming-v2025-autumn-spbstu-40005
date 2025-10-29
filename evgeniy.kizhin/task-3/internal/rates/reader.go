e14d0eb856d739461625d80ade14fae6b023fa0f
u0895732331-alt <u0895732331@gmail.com>
2025-10-29 19:17:44 +0300

    [TASK-3] Implement IO utils

---
+++ evgeniy.kizhin/task-3/internal/rates/reader.go
@@ -0,0 +1,25 @@
+package rates
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"os"
+
+	"evgeniy.kizhin/task-3/internal/model"
+	"golang.org/x/net/html/charset"
+)
+
+func LoadRates(path string) ([]model.Currency, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("read xml: %w", err)
+	}
+	dec := xml.NewDecoder(bytes.NewReader(data))
+	dec.CharsetReader = charset.NewReaderLabel
+	var root model.ValCurs
+	if err := dec.Decode(&root); err != nil {
+		return nil, fmt.Errorf("xml decode: %w", err)
+	}
+	return root.Valute, nil
+}
