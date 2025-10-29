458d9788af123c5bf55bd54ad53192e9243319f4
u0895732331-alt <u0895732331@gmail.com>
2025-10-29 19:08:34 +0300

    [TASK-3] Implement config loader

---
+++ evgeniy.kizhin/task-3/internal/cfg/loader.go
@@ -0,0 +1,25 @@
+package cfg
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func Load(path string) (*Config, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("read config: %w", err)
+	}
+	var c Config
+	if err := yaml.Unmarshal(b, &c); err != nil {
+		return nil, fmt.Errorf("unmarshal config: %w", err)
+	}
+	return &c, nil
+}
