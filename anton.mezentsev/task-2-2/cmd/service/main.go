53d6586d4caf47d7e528fcd919d8efacd70cf4a2
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-31 01:30:07 +0300

    [TASK-2-2] fix lint8

--- anton.mezentsev/task-2-2/cmd/service/main.go
+++ anton.mezentsev/task-2-2/cmd/service/main.go
@@ -70,8 +70,8 @@ func main() {
 	}
 
 	var selectionIndex int
-	_, err = fmt.Scan(&selectionIndex)
 
+	_, err = fmt.Scan(&selectionIndex)
 	if err != nil {
 		fmt.Printf("Invalid preference order: %v\n", err)
 
