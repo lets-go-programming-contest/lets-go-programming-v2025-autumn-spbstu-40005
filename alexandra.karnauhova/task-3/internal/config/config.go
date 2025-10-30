ab57f3edcdf1b8ab96bb2321eece1d9835faded7
“Karnaukhova <masj280906@gmail.com>
2025-10-30 21:29:33 +0300

    [TASK-3] +work with config

---
+++ alexandra.karnauhova/task-3/internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func LoadConfig(thisConfig string) (*Config, error) {
+	data, err := os.ReadFile(thisConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
