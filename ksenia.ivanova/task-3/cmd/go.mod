d9ce27e6489b0c5c59bbfa79dbb19d4afe92e9d3
Robotophile <ksyunya.iv@gmail.com>
2025-10-30 23:54:03 +0300

    [TASK-3] main

---
+++ ksenia.ivanova/task-3/cmd/go.mod
@@ -0,0 +1,6 @@
+
+module ksenia.ivanova/task-3
+
+go 1.22.7
+
+require golang.org/x/text v0.14.0 // indirect
--- ksenia.ivanova/task-3/cmd/service/main.go
+++ ksenia.ivanova/task-3/cmd/service/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"flag"
+)
+
+const (
+	configFlagName    = "config"
+	configFlagDefault = "config.yaml"
+	configFlagUsage   = "path to config file"
+)
+
+func main() {
+	configPath := flag.String(configFlagName, configFlagDefault, configFlagUsage)
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	valutes, err := converter.ParseXMLFile(cfg.InputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	converter.SortByValueDesc(valutes)
+
+	if err := converter.WriteToJSON(valutes, cfg.OutputFile); err != nil {
+		panic(err)
+	}
+}
