cee3fc6e2503d66a165055ddc8e86c65eb62bf67
“Karnaukhova <masj280906@gmail.com>
2025-10-30 21:29:33 +0300

    [TASK-3] +work with config

--- alexandra.karnauhova/task-3/cmd/service/main.go
+++ alexandra.karnauhova/task-3/cmd/service/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"alexandra.karnauhova/task-1/internal/config"
+)
+
+func main() {
+	thisConfig := flag.String("config", "non", "Select a configuration file")
+	flag.Parse()
+
+	if *thisConfig == "non" {
+		panic("Config file is uncorrect")
+	}
+
+	cfg, err := config.LoadConfig(*thisConfig)
+	if err != nil {
+		panic("Failed to load config: " + err.Error())
+	}
 }
