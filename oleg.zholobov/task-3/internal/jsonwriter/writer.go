91eb89fd2ec95dcdb7649bdfdbdc904246630503
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-10-31 19:00:49 +0300

    [TASK-3] added jsonwriter

---
+++ oleg.zholobov/task-3/internal/jsonwriter/writer.go
@@ -0,0 +1,27 @@
+package json_utils
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"oleg.zholobov/task-3/internal/datamodels"
+)
+
+func SaveJSON(path string, currencies []datamodels.Currency) error {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "    ")
+
+	return encoder.Encode(currencies)
+}
