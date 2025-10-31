0911de3f3d1ea6098d5170bbf0234214060f1585
“Karnaukhova <masj280906@gmail.com>
2025-10-31 03:29:46 +0300

    [TASK-3] +work with json

--- alexandra.karnauhova/task-3/cmd/service/main.go
+++ alexandra.karnauhova/task-3/cmd/service/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
+	"path/filepath"
 
 	"alexandra.karnauhova/task-1/internal/config"
 	"alexandra.karnauhova/task-1/internal/parserxml"
+	"alexandra.karnauhova/task-1/internal/sorter"
+	"alexandra.karnauhova/task-1/internal/writer"
 )
 
 func main() {
@@ -20,9 +23,19 @@ func main() {
 		panic("Failed to load config: " + err.Error())
 	}
 
-	currencies, err := parserxml.ParseXML(files.InputFile)
+	valArray, err := parserxml.ParseXML(files.InputFile)
 	if err != nil {
 		panic(err)
 	}
 
+	sortedValutes := sorter.SortByValueDesc(valArray.Valutes)
+
+	outputDir := filepath.Dir(files.OutputFile)
+	if err := writer.CreateDirectory(outputDir); err != nil {
+		panic("Failed to create output directory: " + err.Error())
+	}
+
+	if err := writer.ParseJson(sortedValutes, files.OutputFile); err != nil {
+		panic("Failed to save JSON: " + err.Error())
+	}
 }
---
+++ alexandra.karnauhova/task-3/internal/writer/forJson.go
@@ -0,0 +1,25 @@
+package writer
+
+import (
+	"encoding/json"
+	"os"
+
+	"alexandra.karnauhova/task-1/internal/data"
+)
+
+func CreateDirectory(directory string) error {
+	return os.MkdirAll(directory, 0755)
+}
+
+func ParseJson(valutes []data.Valute, filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	return encoder.Encode(valutes)
+}
