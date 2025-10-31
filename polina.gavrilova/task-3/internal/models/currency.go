960caf86d4ecd8498361edbc3fb7362d9ea9cf89
hahaha_polina <polina.gavrilova2022@mail.ru>
2025-10-31 11:35:45 +0300

    [TASK-3] Fix sorting

--- polina.gavrilova/task-3/internal/models/currency.go
+++ polina.gavrilova/task-3/internal/models/currency.go
@@ -1,21 +1,16 @@
 package models
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+)
 
-type XMLValCurs struct {
-	XMLName xml.Name    `xml:"ValCurs"`
-	Valutes []XMLValute `xml:"Valute"`
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Valutes []Valute `xml:"Valute"`
 }
 
-type XMLValute struct {
-	XMLName  xml.Name `xml:"Valute"`
-	NumCode  string   `xml:"NumCode"`
-	CharCode string   `xml:"CharCode"`
-	Value    string   `xml:"Value"`
-}
-
-type JSONValute struct {
-	NumCode  int     `json:"num_code"`
-	CharCode string  `json:"char_code"`
-	Value    float64 `json:"value"`
+type Valute struct {
+	NumCode  int     `json:"num_code" xml:"NumCode"`
+	CharCode string  `json:"char_code" xml:"CharCode"`
+	Value    float64 `json:"value" xml:"Value"`
 }
