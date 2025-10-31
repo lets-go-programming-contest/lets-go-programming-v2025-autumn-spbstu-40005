1241a28a26e14b80543987bbe72cd5bca108f09d
“Karnaukhova <masj280906@gmail.com>
2025-10-31 04:10:41 +0300

    [TASK-3] Fix lint 2

--- alexandra.karnauhova/task-3/internal/data/infBank.go
+++ alexandra.karnauhova/task-3/internal/data/infBank.go
@@ -21,11 +21,13 @@ type CurrencyValue float64
 
 func (v *CurrencyValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	var str string
+
 	if err := d.DecodeElement(&str, &start); err != nil {
 		return fmt.Errorf("failed to unmarshal currency value: %w", err)
 	}
 
 	str = strings.ReplaceAll(str, ",", ".")
+
 	value, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		return fmt.Errorf("failed to parse currency value: %w", err)
