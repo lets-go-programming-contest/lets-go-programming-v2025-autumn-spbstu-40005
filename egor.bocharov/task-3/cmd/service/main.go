f113071540c2a59ad90ac4c67988912feab5184e
LuckyBichar <enbocharov2006@gmail.com>
2025-10-31 23:27:52 +0300

    [TASK-3] main

--- egor.bocharov/task-3/cmd/service/main.go
+++ egor.bocharov/task-3/cmd/service/main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"flag"
+	"sort"
+
+	"egor.bocharov/task-3/internal/bank"
+	"egor.bocharov/task-3/internal/config"
+	"egor.bocharov/task-3/internal/parser"
+)
+
 func main() {
+	configPath := flag.String("config", "config.yaml", "path to config file")
+	flag.Parse()
+
+	config, err := parser.ParseYAMLFile[config.Config](*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	valCurs, err := parser.ParseXMLFile[bank.ValCurs](config.InputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	sort.Slice(valCurs.Valute, func(i, j int) bool {
+		return valCurs.Valute[i].Value > valCurs.Valute[j].Value
+	})
+
+	if err = parser.EncodeFile(valCurs.Valute, config.OutputFile); err != nil {
+		panic(err)
+	}
 }
