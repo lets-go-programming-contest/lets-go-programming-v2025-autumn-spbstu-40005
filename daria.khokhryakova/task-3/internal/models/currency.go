33f4c52f13200e8099403ade07ab017584ff16ab
DariaKhokhryakova <18.03.2015dasha@gmail.com>
2025-10-31 22:26:34 +0300

    [TASK-3] fix lint

--- daria.khokhryakova/task-3/internal/models/currency.go
+++ daria.khokhryakova/task-3/internal/models/currency.go
@@ -1,9 +1,9 @@
 package models
 
 type CurrencyResult struct {
-	NumCode  int           `xml:"NumCode" json:"num_code"`
+	NumCode  int           `xml:"NumCode"  json:"num_code"`
 	CharCode string        `xml:"CharCode" json:"char_code"`
-	Value    CurrencyValue `xml:"Value" json:"value"`
+	Value    CurrencyValue `xml:"Value"    json:"value"`
 }
 
 type ValCurs struct {
