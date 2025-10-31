6730528ecdd40114edc5e581a15ca8cbdf791aba
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-31 22:24:39 +0300

    [TASK-3] fix lint2

--- anton.mezentsev/task-3/internal/parsers/parsYAML.go
+++ anton.mezentsev/task-3/internal/parsers/parsYAML.go
@@ -1,30 +1,36 @@
 package parsers
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
+var (
+	ErrOpeningFileYAML = errors.New("error opening file")
+	ErrClosingFileYAML = errors.New("error closing file")
+	ErrYAMLDecoding    = errors.New("error yaml decoding")
+)
+
 func ParseYAML[T any](configfilePath string) (*T, error) {
 	file, err := os.Open(configfilePath)
 	if err != nil {
-		return nil, fmt.Errorf("error: opening a file: %w", err)
+		return nil, fmt.Errorf("%w: %w", ErrOpeningFileYAML, err)
 	}
 
 	defer func() {
 		if err := file.Close(); err != nil {
-			panic("error: closing config file")
+			panic(ErrClosingFileYAML)
 		}
 	}()
 
 	var config T
-
 	decoder := yaml.NewDecoder(file)
 
 	if err := decoder.Decode(&config); err != nil {
-		return nil, fmt.Errorf("error: yaml decoding: %w", err)
+		return nil, fmt.Errorf("%w: %w", ErrYAMLDecoding, err)
 	}
 
 	return &config, nil
