14b3a244d1871f70feb87488bf4b10b7466621e9
Щадилов Арсений <tshadilov.a@edu.spbstu.ru>
2025-10-31 22:53:50 +0300

    [TASK-3] initial commit

---
+++ arseniy.shchadilov/task-3/internal/model/model.go
@@ -0,0 +1,49 @@
+package model
+
+import (
+	"encoding/xml"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type CurrencyValue float64
+
+func (v *CurrencyValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var str string
+	if err := d.DecodeElement(&str, &start); err != nil {
+		return fmt.Errorf("failed to unmarshal currency value: %w", err)
+	}
+
+	str = strings.ReplaceAll(str, ",", ".")
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return fmt.Errorf("failed to parse currency value: %w", err)
+	}
+
+	*v = CurrencyValue(value)
+	return nil
+}
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Date    string   `xml:"Date,attr"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+	NumCode  int           `json:"num_code" xml:"NumCode"`
+	CharCode string        `json:"char_code" xml:"CharCode"`
+	Value    CurrencyValue `json:"value" xml:"Value"`
+}
+
+type byValueDesc []Valute
+
+func (v byValueDesc) Len() int           { return len(v) }
+func (v byValueDesc) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
+func (v byValueDesc) Less(i, j int) bool { return v[i].Value > v[j].Value }
+
+func (vc *ValCurs) SortByValueDesc() {
+	sort.Sort(byValueDesc(vc.Valutes))
+}
