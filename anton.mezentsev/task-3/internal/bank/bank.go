fc86ce76434ddf69d7a51b51dc376c6316ac4fb3
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-31 03:44:05 +0300

    [TASK-3] start working

---
+++ anton.mezentsev/task-3/internal/bank/bank.go
@@ -0,0 +1,15 @@
+package bank
+
+type ValCurs struct {
+	Date   string   `xml:"Date,attr"`
+	Name   string   `xml:"name,attr"`
+	Valute []Valute `xml:"Valute"`
+}
+
+type value float64
+
+type Valute struct {
+	NumCode  int    `json:"num_code" xml:"NumCode"`
+	CharCode string `json:"char_code" xml:"CharCode"`
+	Value    value  `json:"value" xml:"Value"`
+}
