ee7809ba7581aaccca2fab660396351d8b36e68b
hahaha_polina <polina.gavrilova2022@mail.ru>
2025-10-31 11:25:10 +0300

    [TASK-3] Fix lint

--- polina.gavrilova/task-3/internal/config/config.go
+++ polina.gavrilova/task-3/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
@@ -12,16 +13,14 @@ type Config struct {
 }
 
 func LoadConfig(path string) (*Config, error) {
-
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read config: %w", err)
 	}
 
 	var cfg Config
-	err = yaml.Unmarshal(data, &cfg)
-	if err != nil {
-		return nil, err
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
 	}
 
 	return &cfg, nil
