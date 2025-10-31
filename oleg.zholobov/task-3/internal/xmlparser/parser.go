c21ac97e6895584c8c965fe8d6792a2271ca5d14
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-10-31 18:43:19 +0300

    [TASK-3] added xmlparser

---
+++ oleg.zholobov/task-3/internal/xmlparser/parser.go
@@ -0,0 +1,29 @@
+package xml_parser
+
+import (
+	"encoding/xml"
+	"io"
+	"os"
+
+	"oleg.zholobov/task-3/internal/datamodels"
+)
+
+func ParseXML(path string) ([]datamodels.Valute, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var valCurs datamodels.ValCurs
+	if err := xml.Unmarshal(data, &valCurs); err != nil {
+		return nil, err
+	}
+
+	return valCurs.Valutes, nil
+}
