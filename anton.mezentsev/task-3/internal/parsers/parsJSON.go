094a55bc245f83ce5452899c6256c43e47a3cfb5
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-31 22:24:39 +0300

    [TASK-3] fix lint2

--- anton.mezentsev/task-3/internal/parsers/parsJSON.go
+++ anton.mezentsev/task-3/internal/parsers/parsJSON.go
@@ -9,26 +9,26 @@ import (
 )
 
 var (
-	ErrCreatingDir  = errors.New("error creating directory")
-	ErrCreatingFile = errors.New("error creating file")
-	ErrEncodingJSON = errors.New("error encoding json")
-	ErrClosingFile  = errors.New("error closing file")
+	ErrCreatingDir     = errors.New("error creating directory")
+	ErrCreatingFile    = errors.New("error creating file")
+	ErrEncodingJSON    = errors.New("error encoding json")
+	ErrClosingFileJSON = errors.New("error closing file")
 )
 
 func SaveToJSON(data any, filePath string) error {
 	dir := filepath.Dir(filePath)
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return fmt.Errorf("%w: %v", ErrCreatingDir, err)
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		return fmt.Errorf("%w: %w", ErrCreatingDir, err)
 	}
 
 	file, err := os.Create(filePath)
 	if err != nil {
-		return fmt.Errorf("%w: %v", ErrCreatingFile, err)
+		return fmt.Errorf("%w: %w", ErrCreatingFile, err)
 	}
 
 	defer func() {
 		if err := file.Close(); err != nil {
-			panic(ErrClosingFile)
+			panic(ErrClosingFileJSON)
 		}
 	}()
 
@@ -36,7 +36,7 @@ func SaveToJSON(data any, filePath string) error {
 	encoder.SetIndent("", " ")
 
 	if err := encoder.Encode(data); err != nil {
-		return fmt.Errorf("%w: %v", ErrEncodingJSON, err)
+		return fmt.Errorf("%w: %w", ErrEncodingJSON, err)
 	}
 
 	return nil
--- anton.mezentsev/task-3/internal/parsers/parsXML.go
+++ anton.mezentsev/task-3/internal/parsers/parsXML.go
@@ -2,31 +2,37 @@ package parsers
 
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"os"
 
 	"golang.org/x/net/html/charset"
 )
 
+var (
+	ErrOpeningFileXML = errors.New("error opening file")
+	ErrClosingFileXML = errors.New("error closing file")
+	ErrXMLDecoding    = errors.New("error xml decoding")
+)
+
 func ParseXML[T any](filePath string) (*T, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("error: opening a file: %w", err)
+		return nil, fmt.Errorf("%w: %w", ErrOpeningFileXML, err)
 	}
 
 	defer func() {
 		if err := file.Close(); err != nil {
-			panic("error: closing xml file")
+			panic(ErrClosingFileXML)
 		}
 	}()
 
 	decoder := xml.NewDecoder(file)
-
 	decoder.CharsetReader = charset.NewReaderLabel
 
 	var result T
 	if err := decoder.Decode(&result); err != nil {
-		return nil, fmt.Errorf("error: xml decoding: %w", err)
+		return nil, fmt.Errorf("%w: %w", ErrXMLDecoding, err)
 	}
 
 	return &result, nil
