84fe524a927311c1fdf20d632ff906d7e4183ae7
MezentsevAnton <fortniter135.anton@mail.ru>
2025-10-31 03:44:05 +0300

    [TASK-3] start working

--- anton.mezentsev/task-3/internal/parsers/parsYAML.go
+++ anton.mezentsev/task-3/internal/parsers/parsYAML.go
@@ -1,1 +1,31 @@
 package parsers
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+func ParseYAML[T any](configfilePath string) (*T, error) {
+	file, err := os.Open(configfilePath)
+	if err != nil {
+		return nil, fmt.Errorf("Error: opening a file: %w", err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic("Error: closing config file")
+		}
+	}()
+
+	var config T
+
+	decoder := yaml.NewDecoder(file)
+
+	if err := decoder.Decode(&config); err != nil {
+		return nil, fmt.Errorf("Error: yaml decoding: %w", err)
+	}
+
+	return &config, nil
+}
