d09652def3239f26f55e5947718610beded17344
“Karnaukhova <masj280906@gmail.com>
2025-11-01 18:31:30 +0300

    [TASK-2-2] Fix comments

--- alexandra.karnauhova/task-2-2/internal/queue/queue.go
+++ alexandra.karnauhova/task-2-2/internal/queue/queue.go
@@ -1,45 +1,42 @@
 package queue
 
-type Element struct {
-	Value    int
-	Priority int
-	Index    int
-}
-
-type Queue []*Element
+type Queue []int
 
 func (q *Queue) Len() int {
 	return len(*q)
 }
 
 func (q *Queue) Less(i, j int) bool {
-	return (*q)[i].Priority > (*q)[j].Priority
+	if i >= q.Len() || j >= q.Len() || i < 0 || j < 0 {
+		panic("indexes out of range")
+	}
+	return (*q)[i] > (*q)[j]
 }
 
 func (q *Queue) Swap(i, j int) {
+	if i >= q.Len() || j >= q.Len() || i < 0 || j < 0 {
+		panic("swapping out of range")
+	}
 	(*q)[i], (*q)[j] = (*q)[j], (*q)[i]
-	(*q)[i].Index = i
-	(*q)[j].Index = j
 }
 
-func (q *Queue) Push(newElement interface{}) {
-	element, oke := newElement.(*Element)
+func (q *Queue) Push(newElement any) {
+	element, oke := newElement.(int)
 	if !oke {
 		return
 	}
 
-	n := len(*q)
-	element.Index = n
 	*q = append(*q, element)
 }
 
-func (q *Queue) Pop() interface{} {
+func (q *Queue) Pop() any {
+	if q.Len() == 0 {
+		return nil
+	}
 	old := *q
 	n := len(old)
 	item := old[n-1]
 
-	old[n-1] = nil
-	item.Index = -1
 	*q = old[0 : n-1]
 
 	return item
