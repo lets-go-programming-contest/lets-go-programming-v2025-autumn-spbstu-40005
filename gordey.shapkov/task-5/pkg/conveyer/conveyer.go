3f89b84d0217fdf254a8b00b2cf915d5ee47974d
TheEmfield <emfield337@gmail.com>
2025-11-20 23:41:48 +0300

    [TASK-5] half of conveyer realisation

--- gordey.shapkov/task-5/pkg/conveyer/conveyer.go
+++ gordey.shapkov/task-5/pkg/conveyer/conveyer.go
@@ -11,36 +11,76 @@ type Pipeline struct {
 }
 
 func New(size int) *Pipeline {
-	return &Pipeline{}
+	return &Pipeline{
+		size:     size,
+		channels: make(map[string]chan string),
+		handlers: []func(ctx context.Context) error{},
+	}
 }
 
-// RegisterDecorator(
-// 	fn func(
-// 		ctx context.Context,
-// 		input chan string,
-// 		output chan string,
-// 	) error,
-// 	input string,
-// 	output string,
-// )
-// RegisterMultiplexer(
-// 	fn func(
-// 		ctx context.Context,
-// 		inputs []chan string,
-// 		output chan string,
-// 	) error,
-// 	inputs []string,
-// 	output string,
-// )
-// RegisterSeparator(
-// 	fn func(
-// 		ctx context.Context,
-// 		input chan string,
-// 		outputs []chan string,
-// 	) error,
-// 	input string,
-// 	outputs []string,
-// )
+func (pipe *Pipeline) register(ch string) chan string {
+	if _, exists := pipe.channels[ch]; !exists {
+		pipe.channels[ch] = make(chan string, pipe.size)
+	}
+
+	return pipe.channels[ch]
+}
+
+func (pipe *Pipeline) RegisterDecorator(
+	fn func(
+		ctx context.Context,
+		input chan string,
+		output chan string,
+	) error,
+	input string,
+	output string,
+) {
+	in := pipe.register(input)
+	out := pipe.register(output)
+	pipe.handlers = append(pipe.handlers, func(ctx context.Context) error {
+		return fn(ctx, in, out)
+	})
+}
+
+func (pipe *Pipeline) RegisterMultiplexer(
+	fn func(
+		ctx context.Context,
+		inputs []chan string,
+		output chan string,
+	) error,
+	inputs []string,
+	output string,
+) {
+	ins := make([]chan string, len(inputs))
+	for i, ch := range inputs {
+		ins[i] = pipe.register(ch)
+	}
+
+	out := pipe.register(output)
+	pipe.handlers = append(pipe.handlers, func(ctx context.Context) error {
+		return fn(ctx, ins, out)
+	})
+}
+func (pipe *Pipeline) RegisterSeparator(
+	fn func(
+		ctx context.Context,
+		input chan string,
+		outputs []chan string,
+	) error,
+	input string,
+	outputs []string,
+) {
+	in := pipe.register(input)
+	outs := make([]chan string, len(outputs))
+	for i, ch := range outputs {
+		outs[i] = pipe.register(ch)
+	}
+
+	pipe.handlers = append(pipe.handlers, func(ctx context.Context) error {
+		return fn(ctx, in, outs)
+	})
+}
+
 // Run(ctx context.Context) error
 // Send(input string, data string) error
 // Recv(output string) (string, error)
