790b998dd7b46693dd5881e3a62d9cf68a16d9ab
Savishna <bravissimo28@mail.ru>
2025-11-22 20:15:13 +0300

    [TASK-5] i hope that

--- eugene.averenkov/task-5/pkg/conveyer/conveyer.go
+++ eugene.averenkov/task-5/pkg/conveyer/conveyer.go
@@ -3,6 +3,127 @@ package conveyer
 import (
 	"context"
 	"errors"
+)
+
+var ErrChanNotFound = errors.New("chan not found")
+
+const undefined = "undefined"
+
+type Conveyer struct {
+	channels map[string]chan string
+	size     int
+	tasks    []func(ctx context.Context) error
+}
+
+func New(size int) *Conveyer {
+	return &Conveyer{
+		channels: make(map[string]chan string),
+		size:     size,
+		tasks:    []func(ctx context.Context) error{},
+	}
+}
+
+func (c *Conveyer) getOrCreateChannel(name string) chan string {
+	if ch, exists := c.channels[name]; exists {
+		return ch
+	}
+	ch := make(chan string, c.size)
+	c.channels[name] = ch
+	return ch
+}
+
+func (c *Conveyer) RegisterDecorator(
+	fn func(ctx context.Context, input chan string, output chan string) error,
+	input string,
+	output string,
+) {
+	inCh := c.getOrCreateChannel(input)
+	outCh := c.getOrCreateChannel(output)
+
+	c.tasks = append(c.tasks, func(ctx context.Context) error {
+		return fn(ctx, inCh, outCh)
+	})
+}
+
+func (c *Conveyer) RegisterMultiplexer(
+	fn func(ctx context.Context, inputs []chan string, output chan string) error,
+	inputs []string,
+	output string,
+) {
+	inChans := make([]chan string, len(inputs))
+	for i, name := range inputs {
+		inChans[i] = c.getOrCreateChannel(name)
+	}
+	outCh := c.getOrCreateChannel(output)
+
+	c.tasks = append(c.tasks, func(ctx context.Context) error {
+		return fn(ctx, inChans, outCh)
+	})
+}
+
+func (c *Conveyer) RegisterSeparator(
+	fn func(ctx context.Context, input chan string, outputs []chan string) error,
+	input string,
+	outputs []string,
+) {
+	inCh := c.getOrCreateChannel(input)
+	outChans := make([]chan string, len(outputs))
+	for i, name := range outputs {
+		outChans[i] = c.getOrCreateChannel(name)
+	}
+
+	c.tasks = append(c.tasks, func(ctx context.Context) error {
+		return fn(ctx, inCh, outChans)
+	})
+}
+
+func (c *Conveyer) Run(ctx context.Context) error {
+	done := make(chan struct{})
+	defer close(done)
+
+	for _, task := range c.tasks {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			if err := task(ctx); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (c *Conveyer) Send(input string, data string) error {
+	ch, exists := c.channels[input]
+	if !exists {
+		return ErrChanNotFound
+	}
+	ch <- data
+	return nil
+}
+
+func (c *Conveyer) Recv(output string) (string, error) {
+	ch, exists := c.channels[output]
+	if !exists {
+		return "", ErrChanNotFound
+	}
+	select {
+	case data, ok := <-ch:
+		if !ok {
+			return undefined, nil
+		}
+		return data, nil
+	default:
+		return "", errors.New("no data")
+	}
+}
+
+/*package conveyer
+
+import (
+	"context"
+	"errors"
 	"sync"
 )
 
@@ -235,3 +356,4 @@ func (c *Conveyer) Close() {
 	c.closeAllChannels()
 	c.running = false
 }
+*/
--- eugene.averenkov/task-5/pkg/handlers/handlers.go
+++ eugene.averenkov/task-5/pkg/handlers/handlers.go
@@ -1,4 +1,114 @@
+// pkg/handlers/handlers.go
 package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var ErrCannotBeDecorated = errors.New("can't be decorated")
+
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+	defer close(output)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, "no decorator") {
+				return ErrCannotBeDecorated
+			}
+
+			if !strings.HasPrefix(data, "decorated: ") {
+				data = "decorated: " + data
+			}
+
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	defer func() {
+		for _, out := range outputs {
+			close(out)
+		}
+	}()
+
+	if len(outputs) == 0 {
+		return nil
+	}
+
+	counter := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			idx := counter % len(outputs)
+			select {
+			case outputs[idx] <- data:
+			case <-ctx.Done():
+				return nil
+			}
+			counter++
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	defer close(output)
+
+	if len(inputs) == 0 {
+		return nil
+	}
+
+	var wg sync.WaitGroup
+	for _, in := range inputs {
+		wg.Add(1)
+		go func(ch chan string) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, ok := <-ch:
+					if !ok {
+						return
+					}
+					if strings.Contains(data, "no multiplexer") {
+						continue
+					}
+					select {
+					case output <- data:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(in)
+	}
+
+	wg.Wait()
+	return nil
+}
+
+/*package handlers
 
 import (
 	"context"
@@ -112,3 +222,4 @@ func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan stri
 
 	return nil
 }
+*/
