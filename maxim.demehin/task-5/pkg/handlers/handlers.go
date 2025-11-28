713e45ec9a4e43b91931c0fd01473468b459bbee
TvoyBatyA12343 <maxdem06@gmail.com>
2025-11-28 12:56:50 +0300

    [TASK-5] prefix decorator handler

--- maxim.demehin/task-5/pkg/handlers/handlers.go
+++ maxim.demehin/task-5/pkg/handlers/handlers.go
@@ -1,1 +1,39 @@
 package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+)
+
+const (
+	decorator_prefix    = "decorated: "
+	no_decorator_prefix = "no decorator"
+)
+
+var ErrCantDecorate = errors.New("can't be decorated")
+
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, no_decorator_prefix) {
+				return ErrCantDecorate
+			}
+
+			if !strings.HasPrefix(data, decorator_prefix) {
+				data = decorator_prefix + data
+			}
+
+			output <- data
+
+			return nil
+		}
+	}
+}
