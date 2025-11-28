5d98e4c176d09eb976415c7165af4b86bd478470
TvoyBatyA12343 <maxdem06@gmail.com>
2025-11-28 14:37:06 +0300

    [TASK-5] fix handlers

--- maxim.demehin/task-5/pkg/handlers/handlers.go
+++ maxim.demehin/task-5/pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"strings"
+	"sync"
 )
 
 const (
@@ -15,6 +16,8 @@ const (
 var ErrCantDecorate = errors.New("can't be decorated")
 
 func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+	defer close(output)
+
 	for {
 		select {
 		case <-ctx.Done():
@@ -32,12 +35,22 @@ func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan str
 				data = decorator_prefix + data
 			}
 
-			output <- data
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
 		}
 	}
 }
 
 func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	defer func() {
+		for _, out := range outputs {
+			close(out)
+		}
+	}()
+
 	if len(outputs) == 0 {
 		return nil
 	}
@@ -58,43 +71,49 @@ func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string
 			select {
 			case outputs[idx] <- data:
 			case <-ctx.Done():
-				return ctx.Err()
-			}
-		}
-	}
-}
-
-func sendData(ctx context.Context, input <-chan string, output chan<- string) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case data, ok := <-input:
-			if !ok {
-				return
-			}
-
-			if !strings.Contains(data, no_multip_str) {
-				select {
-				case output <- data:
-				case <-ctx.Done():
-					return
-				}
+				return nil
 			}
 		}
 	}
 }
 
 func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	defer close(output)
+
 	if len(inputs) == 0 {
 		return nil
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(len(inputs))
+
 	for _, input := range inputs {
-		go sendData(ctx, input, output)
+		go func(in chan string) {
+			defer wg.Done()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, ok := <-in:
+					if !ok {
+						return
+					}
+
+					if strings.Contains(data, no_multip_str) {
+						continue
+					}
+
+					select {
+					case output <- data:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(input)
 	}
 
-	<-ctx.Done()
-
+	wg.Wait()
 	return nil
 }
