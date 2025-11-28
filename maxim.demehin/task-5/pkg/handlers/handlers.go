0d25a6f87d3efefbc86ce70473a39a499e4a6c42
TvoyBatyA12343 <maxdem06@gmail.com>
2025-11-28 13:18:25 +0300

    [TASK-5] separator func

--- maxim.demehin/task-5/pkg/handlers/handlers.go
+++ maxim.demehin/task-5/pkg/handlers/handlers.go
@@ -9,6 +9,7 @@ import (
 const (
 	decorator_prefix    = "decorated: "
 	no_decorator_prefix = "no decorator"
+	no_multip_str = "no multiplexer"
 )
 
 var ErrCantDecorate = errors.New("can't be decorated")
@@ -34,6 +35,47 @@ func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan str
 			output <- data
 
 			return nil
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	out_len := len(outputs)
+
+	if out_len == 0 {
+		return nil
+	}
+
+	cnt := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			out_idx := cnt % out_len
+
+			cnt++
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case outputs[out_idx] <- data:
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case 
 		}
 	}
 }
