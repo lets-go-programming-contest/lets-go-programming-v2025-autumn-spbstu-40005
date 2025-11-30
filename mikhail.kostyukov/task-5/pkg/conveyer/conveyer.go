863415c7d16857466c70408f267309c70cf2b29e
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-11-30 15:17:40 +0300

    [TASK-5] Added handler register logic & channel management

--- mikhail.kostyukov/task-5/pkg/conveyer/conveyer.go
+++ mikhail.kostyukov/task-5/pkg/conveyer/conveyer.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 )
 
-const UndefinedResult = "undefined"
+const undefinedResult = "undefined"
 
 var ErrChannelNotFound = errors.New("chan not found")
 
@@ -50,8 +50,75 @@ func (p *Pipeline) Recv(outputName string) (string, error) {
 
 	data, ok := <- ch
 	if !ok {
-		return UndefinedResult, nil
+		return undefinedResult, nil
 	}
 
 	return data, nil
+}
+
+func (p *Pipeline) ensureChannelExists(name string) chan string {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	if ch, exists := p.channels[name]; exists {
+		return ch
+	}
+
+	newChannel := make(chan string, p.channelBuffer)
+	p.channels[name] = newChannel
+
+	return newChannel
+}
+
+func (p *Pipeline) RegisterDecorator(
+	workerFunc func(context.Context, chan string, chan string) error,
+	sourceName string,
+	destName string,
+) {
+	sourceChan := p.ensureChannelExists(sourceName)
+	destChan := p.ensureChannelExists(destName)
+
+	task := func(ctx context.Context) error {
+		return workerFunc(ctx, sourceChan, destChan)
+	}
+
+	p.tasks = append(p.tasks, task)
+}
+
+func (p *Pipeline) RegisterMultiplexer(
+	workerFunc func(context.Context, []chan string, chan string) error,
+	sourceNames []string,
+	destName string,
+) {
+	sources := make([]chan string, len(sourceNames))
+	for i, name := range sourceNames {
+		sources[i] = p.ensureChannelExists(name)
+	}
+
+	destChan := p.ensureChannelExists(destName)
+
+	task := func(ctx context.Context) error {
+		return workerFunc(ctx, sources, destChan)
+	}
+
+	p.tasks = append(p.tasks, task)
+}
+
+func (p *Pipeline) RegisterSeparator(
+	workerFunc func(context.Context, chan string, []chan string) error,
+	sourceName string,
+	destNames []string,
+) {
+	sourceChan := p.ensureChannelExists(sourceName)
+
+	destinations := make([]chan string, len(destNames))
+	for i, name := range destNames {
+		destinations[i] = p.ensureChannelExists(name)
+	}
+
+	task := func(ctx context.Context) error {
+		return workerFunc(ctx, sourceChan, destinations)
+	}
+
+	p.tasks = append(p.tasks, task)
 }
