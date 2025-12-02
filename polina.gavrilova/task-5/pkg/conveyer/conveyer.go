2acaf36458512c109102157f52201f8f68c84f66
hahaha_polina <polina.gavrilova2022@mail.ru>
2025-12-02 17:04:13 +0300

    [TASK-5] Add basic pipeline struct and New function

--- polina.gavrilova/task-5/pkg/conveyer/conveyer.go
+++ polina.gavrilova/task-5/pkg/conveyer/conveyer.go
@@ -1,1 +1,34 @@
 package conveyer
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+var ErrChanNotFound = errors.New("channel not found")
+
+type conveyer interface {
+	RegisterDecorator(fn func(context.Context, chan string, chan string) error, input, output string)
+	RegisterMultiplexer(fn func(context.Context, []chan string, chan string) error, inputs []string, output string)
+	RegisterSeparator(fn func(context.Context, chan string, []chan string) error, input string, outputs []string)
+	Run(ctx context.Context) error
+	Send(input string, data string) error
+	Recv(output string) (string, error)
+}
+
+type pipeline struct {
+	bufferSize int
+	channels   map[string]chan string
+	workers    []func(context.Context) error
+	mutex      sync.RWMutex
+}
+
+func New(size int) *pipeline {
+	return &pipeline{
+		bufferSize: size,
+		channels:   make(map[string]chan string),
+		workers:    make([]func(context.Context) error, 0),
+		mutex:      sync.RWMutex{},
+	}
+}
