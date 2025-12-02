e5abf0354ec4160652e809b7fa93f85e9a0561c2
hahaha_polina <polina.gavrilova2022@mail.ru>
2025-12-02 17:21:29 +0300

    [TASK-5] Add handler functions

--- polina.gavrilova/task-5/pkg/handlers/handlers.go
+++ polina.gavrilova/task-5/pkg/handlers/handlers.go
@@ -1,1 +1,110 @@
 package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+const (
+	prefixToAdd   = "decorated: "
+	triggerNoDeco = "no decorator"
+	triggerNoMult = "no multiplexer"
+)
+
+var (
+	ErrCantDecorate = errors.New("can't be decorated")
+	ErrEmptyOutputs = errors.New("empty outputs")
+)
+
+func PrefixDecoratorFunc(ctx context.Context, input, output chan string) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, triggerNoDeco) {
+				return ErrCantDecorate
+			}
+
+			if !strings.HasPrefix(data, prefixToAdd) {
+				data = prefixToAdd + data
+			}
+
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	if len(inputs) == 0 {
+		return ErrEmptyOutputs
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(inputs))
+
+	for _, inCh := range inputs {
+		go func(ch chan string) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, ok := <-ch:
+					if !ok {
+						return
+					}
+					if strings.Contains(data, triggerNoMult) {
+						continue
+					}
+					select {
+					case output <- data:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(inCh)
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	if len(outputs) == 0 {
+		return ErrEmptyOutputs
+	}
+
+	currentIndex := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			targetChan := outputs[currentIndex]
+			currentIndex = (currentIndex + 1) % len(outputs)
+
+			select {
+			case targetChan <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
