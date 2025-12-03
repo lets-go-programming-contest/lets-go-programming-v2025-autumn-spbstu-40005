735c1c1a05306c7bc072cace867e871b0cda7fa8
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-12-03 22:59:10 +0300

    [TASK-5] implement PrefixDecoratorFunc handler with error rules and prefix logic

--- oleg.zholobov/task-5/pkg/handlers/handlers.go
+++ oleg.zholobov/task-5/pkg/handlers/handlers.go
@@ -1,1 +1,36 @@
 package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+)
+
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case v, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(v, "no decorator") {
+				return errors.New("can't be decorated")
+			}
+
+			if !strings.HasPrefix(v, "decorated: ") {
+				v = "decorated: " + v
+			}
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case output <- v:
+			}
+		}
+	}
+}
