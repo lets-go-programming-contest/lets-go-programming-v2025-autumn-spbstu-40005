6c78dd80040e6ddec47d200b97a64aca5f1a59df
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-12-03 22:43:41 +0300

    [TASK-5] implemented Run

--- oleg.zholobov/task-5/pkg/conveyer/conveyer.go
+++ oleg.zholobov/task-5/pkg/conveyer/conveyer.go
@@ -111,8 +111,97 @@ func (c *Conveyer) RegisterSeparator(
 }
 
 func (c *Conveyer) Run(ctx context.Context) error {
-	<-ctx.Done()
-	return nil
+	var wg sync.WaitGroup
+
+	errCh := make(chan error, 1)
+
+	runners := c.buildRunners()
+
+	for _, r := range runners {
+		wg.Add(1)
+		go func(run runFunc) {
+			defer wg.Done()
+			if err := run(ctx); err != nil {
+				select {
+				case errCh <- err:
+				default:
+				}
+			}
+		}(r)
+	}
+
+	var finalErr error
+
+	select {
+	case <-ctx.Done():
+		finalErr = ctx.Err()
+	case err := <-errCh:
+		finalErr = err
+	}
+
+	wg.Wait()
+
+	c.closeAllChannels()
+
+	return finalErr
+}
+
+func (c *Conveyer) buildRunners() []runFunc {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	var runners []runFunc
+
+	// Decorators
+	for _, d := range c.decorators {
+		input := c.channels[d.input]
+		output := c.channels[d.output]
+		fn := d.fn
+		runners = append(runners, func(ctx context.Context) error {
+			return fn(ctx, input, output)
+		})
+	}
+
+	// Multiplexers
+	for _, m := range c.multiplexers {
+		var inputs []chan string
+		for _, name := range m.inputs {
+			inputs = append(inputs, c.channels[name])
+		}
+		output := c.channels[m.output]
+		fn := m.fn
+		runners = append(runners, func(ctx context.Context) error {
+			return fn(ctx, inputs, output)
+		})
+	}
+
+	// Separators
+	for _, s := range c.separators {
+		input := c.channels[s.input]
+		var outputs []chan string
+		for _, name := range s.outputs {
+			outputs = append(outputs, c.channels[name])
+		}
+		fn := s.fn
+		runners = append(runners, func(ctx context.Context) error {
+			return fn(ctx, input, outputs)
+		})
+	}
+
+	return runners
+}
+
+func (c *Conveyer) closeAllChannels() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for name, ch := range c.channels {
+		func() {
+			defer func() { _ = recover() }()
+			close(ch)
+			_ = name
+		}()
+	}
 }
 
 func (c *Conveyer) Send(input string, data string) error {
@@ -144,3 +233,5 @@ func (c *Conveyer) Recv(output string) (string, error) {
 
 	return val, nil
 }
+
+type runFunc func(ctx context.Context) error
