574181a07006aa6955ef3b86faf9bf9eac9fd219
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-12-03 22:10:01 +0300

    [TASK-5] add channel initialization and registration logic with working Send/Recv

--- oleg.zholobov/task-5/pkg/conveyer/conveyer.go
+++ oleg.zholobov/task-5/pkg/conveyer/conveyer.go
@@ -1,1 +1,146 @@
 package conveyer
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+type decoratorConfig struct {
+	fn     func(ctx context.Context, input chan string, output chan string) error
+	input  string
+	output string
+}
+
+type multiplexerConfig struct {
+	fn     func(ctx context.Context, inputs []chan string, output chan string) error
+	inputs []string
+	output string
+}
+
+type separatorConfig struct {
+	fn      func(ctx context.Context, input chan string, outputs []chan string) error
+	input   string
+	outputs []string
+}
+
+type Conveyer struct {
+	size int
+
+	channels map[string]chan string
+
+	decorators   []decoratorConfig
+	multiplexers []multiplexerConfig
+	separators   []separatorConfig
+
+	mu sync.RWMutex
+}
+
+func New(size int) *Conveyer {
+	return &Conveyer{
+		size:     size,
+		channels: make(map[string]chan string),
+	}
+}
+
+func (c *Conveyer) getOrCreateChannel(name string) chan string {
+	ch, ok := c.channels[name]
+	if !ok {
+		ch = make(chan string, c.size)
+		c.channels[name] = ch
+	}
+	return ch
+}
+
+func (c *Conveyer) RegisterDecorator(
+	fn func(ctx context.Context, input chan string, output chan string) error,
+	input string,
+	output string,
+) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.getOrCreateChannel(input)
+	c.getOrCreateChannel(output)
+
+	c.decorators = append(c.decorators, decoratorConfig{
+		fn:     fn,
+		input:  input,
+		output: output,
+	})
+}
+
+func (c *Conveyer) RegisterMultiplexer(
+	fn func(ctx context.Context, inputs []chan string, output chan string) error,
+	inputs []string,
+	output string,
+) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, in := range inputs {
+		c.getOrCreateChannel(in)
+	}
+	c.getOrCreateChannel(output)
+
+	c.multiplexers = append(c.multiplexers, multiplexerConfig{
+		fn:     fn,
+		inputs: inputs,
+		output: output,
+	})
+}
+
+func (c *Conveyer) RegisterSeparator(
+	fn func(ctx context.Context, input chan string, outputs []chan string) error,
+	input string,
+	outputs []string,
+) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.getOrCreateChannel(input)
+	for _, out := range outputs {
+		c.getOrCreateChannel(out)
+	}
+
+	c.separators = append(c.separators, separatorConfig{
+		fn:      fn,
+		input:   input,
+		outputs: outputs,
+	})
+}
+
+func (c *Conveyer) Run(ctx context.Context) error {
+	<-ctx.Done()
+	return nil
+}
+
+func (c *Conveyer) Send(input string, data string) error {
+	c.mu.RLock()
+	ch, ok := c.channels[input]
+	c.mu.RUnlock()
+
+	if !ok {
+		return errors.New("chan not found")
+	}
+
+	ch <- data
+	return nil
+}
+
+func (c *Conveyer) Recv(output string) (string, error) {
+	c.mu.RLock()
+	ch, ok := c.channels[output]
+	c.mu.RUnlock()
+
+	if !ok {
+		return "", errors.New("chan not found")
+	}
+
+	val, ok := <-ch
+	if !ok {
+		return "undefined", nil
+	}
+
+	return val, nil
+}
