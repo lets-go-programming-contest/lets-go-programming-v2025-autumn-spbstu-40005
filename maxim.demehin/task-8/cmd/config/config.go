4877e6b27c6fab94a0a2d9e7dc5dae9e29b900b5
TvoyBatyA12343 <maxdem06@gmail.com>
2025-12-04 17:21:14 +0300

    [TASK-8] config struct impl

---
+++ maxim.demehin/task-8/cmd/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	LogLevel    string `yaml:"log_level"`
+}
+
+func parseConfig(data []byte) (*Config, error) {
+	var config Config
+
+	err := yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, fmt.Errorf("error while parsing config: %w", err)
+	}
+
+	return &config, nil
+}
+
+func (c *Config) PrintConfig() {
+	fmt.Printf("%s %s\n", c.Environment, c.LogLevel)
+}
