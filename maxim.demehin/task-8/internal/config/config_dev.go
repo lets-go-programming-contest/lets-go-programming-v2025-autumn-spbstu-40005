05e4d3e49693498ab8bd36000066f53b3305d39e
TvoyBatyA12343 <maxdem06@gmail.com>
2025-12-04 17:32:15 +0300

    [TASK-8] change loading logic

--- maxim.demehin/task-8/internal/config/config_dev.go
+++ maxim.demehin/task-8/internal/config/config_dev.go
@@ -7,11 +7,6 @@ import _ "embed"
 //go:embed dev.yaml
 var configData []byte
 
-func init() {
-	cfg, err := parseConfig(configData)
-	if err != nil {
-		panic(err)
-	}
-
-	cfg.PrintConfig()
+func Load() (*Config, error) {
+	return parseConfig(configData)
 }
