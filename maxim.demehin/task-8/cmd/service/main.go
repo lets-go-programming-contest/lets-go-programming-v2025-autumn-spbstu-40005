ad5e5f5eab74074b4bdcdabe201548d5a511dbd8
TvoyBatyA12343 <maxdem06@gmail.com>
2025-12-04 17:42:32 +0300

    [TASK-8] install needed packages + done task

--- maxim.demehin/task-8/cmd/service/main.go
+++ maxim.demehin/task-8/cmd/service/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/TvoyBatyA12343/task-8/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Println("failed config loading: %w", err)
+
+		return
+	}
+
+	cfg.PrintConfig()
 }
