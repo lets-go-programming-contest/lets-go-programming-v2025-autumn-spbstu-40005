df9e85eeb7042e610701f90a39a8d7d64e09e349
smirnov-vladislav <smirnovvladislav505@gmail.com>
2025-12-04 17:16:23 +0300

    [TASK-5] initial commit

---
+++ vladislav.smirnov/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,110 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	ErrCantDecorate = errors.New("can't be decorated")
+	ErrEmptyChannel = errors.New("empty channel")
+)
+
+func DecoratorFunc(ctx context.Context, input, output chan string) error {
+	const prefix = "decorated: "
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case value, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(value, "no decorator") {
+				return ErrCantDecorate
+			}
+			if !strings.HasPrefix(value, prefix) {
+				value = prefix + value
+			}
+
+			select {
+			case output <- value:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	if len(outputs) == 0 {
+		return ErrEmptyChannel
+	}
+
+	index := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case value, ok := <-input:
+			if !ok {
+				return nil
+			}
+			select {
+			case outputs[index] <- value:
+				index = (index + 1) % len(outputs)
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	if len(inputs) == 0 {
+		return ErrEmptyChannel
+	}
+
+	var wg sync.WaitGroup
+	errGroup, gCtx := errgroup.WithContext(ctx)
+
+	for _, channel := range inputs {
+		channel := channel
+		wg.Add(1)
+		errGroup.Go(func() error {
+			defer wg.Done()
+			for {
+				select {
+				case <-gCtx.Done():
+					return gCtx.Err()
+				case value, ok := <-channel:
+					if !ok {
+						return nil
+					}
+
+					if strings.Contains(value, "no multiplexer") {
+						continue
+					}
+
+					select {
+					case output <- value:
+					case <-gCtx.Done():
+						return gCtx.Err()
+					}
+				}
+			}
+		})
+	}
+	err := errGroup.Wait()
+
+	if err != nil && errors.Is(err, context.Canceled) {
+		return nil
+	}
+
+	return err
+}
