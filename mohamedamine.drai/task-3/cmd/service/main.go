dc27443f8952ac132eab160ade1d7a2bb1195dfc
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-12-04 17:11:41 +0300

    [TASK-3] (#91)
    * [TASK-3] Complete solution
    
    * [TASK-3] Complete solution
    
    * [TASK-3] Complete solution
    
    * [TASK-3] Complete solution
    
    * [TASK-3] Complete solution
    
    * [TASK-3] Complete solution
    
    * [TASK-3] Fix
    
    * [TASK-3] Fix
    
    * [TASK-3] Fix
    
    * [TASK-3] Fix
    
    * [TASK-3] Fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] fix
    
    * [TASK-3] Last Fix
    
    * [TASK-3] Last Fix
    
    * [TASK-3] Final fix
    
    * [TASK-3] Final fix
    
    * [TASK-3] Final fix
    
    * [TASK-3] Final fix

---
+++ mohamedamine.drai/task-3/cmd/service/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"sort"
+
+	"mohamedamine.drai/task-3/internal/config"
+	"mohamedamine.drai/task-3/internal/converter"
+	"mohamedamine.drai/task-3/internal/jsonwriter"
+	"mohamedamine.drai/task-3/internal/xmlparser"
+)
+
+const (
+	dirPermissions  = 0o755
+	filePermissions = 0o600
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "path to config")
+	flag.Parse()
+
+	config, err := config.LoadConfig(*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	currencyList := converter.Rates{Data: []converter.Currency{}}
+
+	err = xmlparser.ReadXML(config.InputFile, &currencyList)
+	if err != nil {
+		panic(err)
+	}
+
+	sort.Slice(currencyList.Data, func(i, j int) bool {
+		return currencyList.Data[i].Value > currencyList.Data[j].Value
+	})
+
+	err = jsonwriter.SaveToJSON(config.OutputFile, currencyList.Data, dirPermissions, filePermissions)
+	if err != nil {
+		panic(err)
+	}
+}
