cadf9d11c961b2bff804675f5ccc9c10fb92b01f
u0895732331-alt <u0895732331@gmail.com>
2025-12-05 22:36:53 +0300

    [TASK-5] Implement conveyer

---
+++ evgeniy.kizhin/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,164 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+const Undefined = "undefined"
+
+var ErrChanNotFound = errors.New("chan not found")
+
+type Conveyor struct {
+	mu     sync.RWMutex
+	chans  map[string]chan string
+	tasks  []func(context.Context) error
+	buffer int
+}
+
+func New(size int) *Conveyor {
+	return &Conveyor{
+		mu:     sync.RWMutex{},
+		chans:  make(map[string]chan string),
+		tasks:  make([]func(context.Context) error, 0),
+		buffer: size,
+	}
+}
+
+func (c *Conveyor) get(name string) (chan string, bool) {
+	c.mu.RLock()
+	inch, ok := c.chans[name]
+	c.mu.RUnlock()
+
+	return inch, ok
+}
+
+func (c *Conveyor) getOrCreate(name string) chan string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if inch, ok := c.chans[name]; ok {
+		return inch
+	}
+
+	ch := make(chan string, c.buffer)
+	c.chans[name] = ch
+
+	return ch
+}
+
+func (c *Conveyor) RegisterDecorator(
+	handlerFn func(ctx context.Context, input chan string, output chan string) error,
+	input string,
+	output string,
+) {
+	inch := c.getOrCreate(input)
+	out := c.getOrCreate(output)
+	task := func(ctx context.Context) error {
+		return handlerFn(ctx, inch, out)
+	}
+
+	c.mu.Lock()
+	c.tasks = append(c.tasks, task)
+	c.mu.Unlock()
+}
+
+func (c *Conveyor) RegisterMultiplexer(
+	handlerFn func(ctx context.Context, inputs []chan string, output chan string) error,
+	inputs []string,
+	output string,
+) {
+	ins := make([]chan string, 0, len(inputs))
+	for _, name := range inputs {
+		ins = append(ins, c.getOrCreate(name))
+	}
+
+	out := c.getOrCreate(output)
+
+	task := func(ctx context.Context) error {
+		return handlerFn(ctx, ins, out)
+	}
+
+	c.mu.Lock()
+	c.tasks = append(c.tasks, task)
+	c.mu.Unlock()
+}
+
+func (c *Conveyor) RegisterSeparator(
+	handlerFn func(ctx context.Context, input chan string, outputs []chan string) error,
+	input string,
+	outputs []string,
+) {
+	inch := c.getOrCreate(input)
+	outs := make([]chan string, 0, len(outputs))
+
+	for _, name := range outputs {
+		outs = append(outs, c.getOrCreate(name))
+	}
+
+	task := func(ctx context.Context) error {
+		return handlerFn(ctx, inch, outs)
+	}
+
+	c.mu.Lock()
+	c.tasks = append(c.tasks, task)
+	c.mu.Unlock()
+}
+
+func (c *Conveyor) Run(ctx context.Context) error {
+	c.mu.RLock()
+	group, gctx := errgroup.WithContext(ctx)
+
+	for i := range c.tasks {
+		task := c.tasks[i]
+
+		group.Go(func() error {
+			return task(gctx)
+		})
+	}
+	c.mu.RUnlock()
+
+	err := group.Wait()
+
+	c.mu.Lock()
+	for _, ch := range c.chans {
+		close(ch)
+	}
+
+	c.mu.Unlock()
+
+	if err != nil {
+		return fmt.Errorf("pipeline error: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Conveyor) Send(input string, data string) error {
+	inch, ok := c.get(input)
+	if !ok {
+		return ErrChanNotFound
+	}
+
+	inch <- data
+
+	return nil
+}
+
+func (c *Conveyor) Recv(output string) (string, error) {
+	inch, found := c.get(output)
+	if !found {
+		return "", ErrChanNotFound
+	}
+
+	msg, found := <-inch
+	if !found {
+		return Undefined, nil
+	}
+
+	return msg, nil
+}
