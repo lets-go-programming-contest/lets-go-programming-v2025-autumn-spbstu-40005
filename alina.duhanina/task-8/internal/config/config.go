6b1ce7be8d06e08ca1c4764c40ca2f209ba57df5
alina <duhaninaalina3@gmail.com>
2025-12-06 20:06:11 +0300

    [TASK-8] try

--- alina.duhanina/task-8/internal/config/config.go
+++ alina.duhanina/task-8/internal/config/config.go
@@ -23,6 +23,12 @@ func parseConfig(data []byte) (*Config, error) {
 		return nil, fmt.Errorf("error while parsing config: %w", err)
 	}
 
+	if config.Environment == "" {
+		return nil, fmt.Errorf("environment field is empty")
+	}
+	if config.LogLevel == "" {
+		return nil, fmt.Errorf("log_level field is empty")
+	}
 	return &config, nil
 }
 
