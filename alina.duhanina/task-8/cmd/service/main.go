1e632e959c24d00fc47632a829933266bd2faaab
alina <duhaninaalina3@gmail.com>
2025-12-06 18:01:46 +0300

    [TASK-8] fix conf

--- alina.duhanina/task-8/cmd/service/main.go
+++ alina.duhanina/task-8/cmd/service/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"alina.duhanina/task-8/internal/config"
+	"fmt"
 )
 
 func main() {
 	cfg, err := config.GetConfig()
 	if err != nil {
 		fmt.Println("failed loading: %w", err)
+
+		return
 	}
 
 	fmt.Print(cfg.Environment + " " + cfg.LogLevel)
