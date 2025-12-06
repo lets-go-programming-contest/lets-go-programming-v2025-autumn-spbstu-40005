50f1eb59df41c47223aa64e7633a3ebac682a08f
alina <duhaninaalina3@gmail.com>
2025-12-06 17:37:35 +0300

    [TASK-8] fix conf

--- alina.duhanina/task-8/internal/config/config_dev.go
+++ alina.duhanina/task-8/internal/config/config_dev.go
@@ -1,13 +1,9 @@
-//go:build dev
-// +build dev
+//go:build dev || (!dev && !prod)
 
 package config
 
 import _ "embed"
 
-//go:embed dev.yaml
-var configData []byte
-
 func GetConfig() (*Config, error) {
-	return parseConfig(configData)
+	return parseConfig(devConfigData)
 }
--- alina.duhanina/task-8/internal/config/config_prod.go
+++ alina.duhanina/task-8/internal/config/config_prod.go
@@ -1,13 +1,9 @@
-//go:build !dev
-// +build !dev
+//go:build prod
 
 package config
 
 import _ "embed"
 
-//go:embed prod.yaml
-var configData []byte
-
 func GetConfig() (*Config, error) {
-	return parseConfig(configData)
+	return parseConfig(prodConfigData)
 }
