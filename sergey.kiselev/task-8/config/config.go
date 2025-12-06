f42f3d80763af8f6211a6016d5eba8aa7acc5da7
deonik3 <kis.sergey2006@gmail.com>
2025-12-06 16:43:54 +0300

    [TASK-8] create struct for config and parse config

---
+++ sergey.kiselev/task-8/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	LogLevel    string `yaml:"log_level"`
+}
+
+func parseConfig(data []byte) (*Config, error) {
+	var cfg Config
+	err := yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
