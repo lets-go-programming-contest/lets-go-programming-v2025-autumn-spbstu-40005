3196c7a885d92d134fdc6a367a79f0c89833fdac
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-12-06 19:59:35 +0300

    [TASK-8] Added config-parse logic & created base files

---
+++ mikhail.kostyukov/task-8/internal/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig() (*Config, error) {
+	var config Config
+
+	if err := yaml.Unmarshal(configFile, &config); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal config: %w", err)
+	}
+
+	return &config, nil
+}
