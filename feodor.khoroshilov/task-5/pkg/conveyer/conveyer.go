e06a211e9aff4fe8bd7545058588e7172974d6cc
Drain-Doglazer <witchbtch64@gmail.com>
2025-12-11 10:54:01 +0300

    [TASK-5] dofixsil 3

--- feodor.khoroshilov/task-5/pkg/conveyer/conveyer.go
+++ feodor.khoroshilov/task-5/pkg/conveyer/conveyer.go
@@ -152,7 +152,7 @@ func (c *conveyer) closeAll() {
 }
 
 func (c *conveyer) Send(name string, data string) error {
-	ch, exists := c.getChannel(name)
+	ch, err := c.getChannel(name)
 	if !exists {
 		return ErrChannelNotFound
 	}
@@ -163,7 +163,7 @@ func (c *conveyer) Send(name string, data string) error {
 }
 
 func (c *conveyer) Recv(name string) (string, error) {
-	ch, exists := c.getChannel(name)
+	ch, err := c.getChannel(name)
 	if !exists {
 		return "", ErrChannelNotFound
 	}
