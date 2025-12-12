3829472a722c9607f7e144e4814092445996b9a8
Brev1ch <flirktv@gmail.com>
2025-12-12 15:03:20 +0300

    [TASK-5] linter fix

--- nikita.brevnov/task-5/pkg/handlers/handlers.go
+++ nikita.brevnov/task-5/pkg/handlers/handlers.go
@@ -73,6 +73,7 @@ func MultiplexerFunc(ctx context.Context, ins []chan string, out chan string) er
 	var waitGroup sync.WaitGroup
 
 	waitGroup.Add(len(ins))
+
 	for _, inputChan := range ins {
 		go func(inputCh chan string) {
 			defer waitGroup.Done()
