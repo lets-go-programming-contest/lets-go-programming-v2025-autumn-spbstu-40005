4bf9ad2d321217e4f45c2e0e58bc81a4b4191ff7
TvoyBatyA12343 <maxdem06@gmail.com>
2025-12-14 18:53:35 +0300

    [TASK-6] some tests for wifi

---
+++ maxim.demehin/task-6/internal/wifi/wifi_test.go
@@ -0,0 +1,40 @@
+package wifi_test
+
+import (
+	"errors"
+	"net"
+	"testing"
+
+	myWifi "github.com/TvoyBatyA1234/task-6/internal/wifi"
+	"github.com/mdlayher/wifi"
+	"github.com/stretchr/testify/require"
+)
+
+//go:generate mockery --all --testonly --quiet --outpkg wifi_test --output .
+
+var TestErr = errors.New("test error")
+
+func createMAC(s string) net.HardwareAddr {
+	addr, _ := net.ParseMAC(s)
+	return addr
+}
+
+func TestRetrieveMACs_Successful(t *testing.T) {
+	t.Parallel()
+
+	mockHandler := NewWiFiHandle(t)
+	service := myWifi.New(mockHandler)
+
+	networkIfaces := []*wifi.Interface{
+		{Name: "wifi0", HardwareAddr: createMAC("11:22:33:44:55:66")},
+		{Name: "wifi1", HardwareAddr: createMAC("aa:bb:cc:dd:ee:ff")},
+	}
+	mockHandler.On("Interfaces").Return(networkIfaces, nil)
+
+	macs, err := service.GetAddresses()
+	require.NoError(t, err)
+	require.Equal(t, []net.HardwareAddr{
+		createMAC("11:22:33:44:55:66"),
+		createMAC("aa:bb:cc:dd:ee:ff"),
+	}, macs)
+}
