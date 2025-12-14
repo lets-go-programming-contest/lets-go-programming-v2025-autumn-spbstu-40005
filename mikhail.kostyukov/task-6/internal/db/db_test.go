7ce5416581b305cd2992762c7b7808d1c0efa49c
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-12-14 14:58:04 +0300

    [TASK-6] Added tests for wifi

---
+++ mikhail.kostyukov/task-6/internal/db/db_test.go
@@ -0,0 +1,9 @@
+package db_test
+
+import (
+	"testing"
+
+	"github.com/KostyukovMichael/lets-go-programming-v2025-autumn-spbstu-40005/task-6/internal/db"
+)
+
+func TestGetNames()
---
+++ mikhail.kostyukov/task-6/internal/wifi/wifi_test.go
@@ -0,0 +1,143 @@
+package wifi_test
+
+import (
+	"errors"
+	"net"
+	"testing"
+
+	"github.com/mdlayher/wifi"
+	"github.com/stretchr/testify/require"
+
+	myWiFi "github.com/KostyukovMichael/lets-go-programming-v2025-autumn-spbstu-40005/task-6/internal/wifi"
+)
+
+//go:generate mockery --all --testonly --quiet --outpkg wifi_test --output .
+
+var ErrExpected = errors.New("expected error")
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	mockWiFi := NewWiFiHandle(t)
+	service := myWiFi.New(mockWiFi)
+
+	require.Equal(t, mockWiFi, service.WiFi)
+}
+
+func parseMAC(s string) net.HardwareAddr {
+	hwAddr, _ := net.ParseMAC(s)
+
+	return hwAddr
+}
+
+func TestGetAddresses(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name                 string
+		mockReturnInterfaces []*wifi.Interface
+		mockReturnError      error
+		expectedAddrs        []net.HardwareAddr
+		expectError          bool
+	}{
+		{
+			name: "success",
+			mockReturnInterfaces: []*wifi.Interface{
+				{Name: "wlan0", HardwareAddr: parseMAC("00:11:22:33:44:55")},
+				{Name: "eth0", HardwareAddr: parseMAC("aa:bb:cc:dd:ee:ff")},
+			},
+			mockReturnError: nil,
+			expectedAddrs: []net.HardwareAddr{
+				parseMAC("00:11:22:33:44:55"),
+				parseMAC("aa:bb:cc:dd:ee:ff"),
+			},
+			expectError: false,
+		},
+		{
+			name:                 "error from interfaces",
+			mockReturnInterfaces: nil,
+			mockReturnError:      ErrExpected,
+			expectedAddrs:        nil,
+			expectError:          true,
+		},
+		{
+			name:                 "empty interfaces",
+			mockReturnInterfaces: []*wifi.Interface{},
+			mockReturnError:      nil,
+			expectedAddrs:        []net.HardwareAddr{},
+			expectError:          false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			mockWiFi := NewWiFiHandle(t)
+			service := myWiFi.New(mockWiFi)
+
+			mockWiFi.On("Interfaces").Return(tc.mockReturnInterfaces, tc.mockReturnError)
+			addrs, err := service.GetAddresses()
+
+			if tc.expectError {
+				require.Error(t, err)
+				require.ErrorIs(t, err, tc.mockReturnError)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedAddrs, addrs)
+			}
+		})
+	}
+}
+
+func TestGetNames(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name                 string
+		mockReturnInterfaces []*wifi.Interface
+		mockReturnError      error
+		expectedNames        []string
+		expectError          bool
+	}{
+		{
+			name: "success",
+			mockReturnInterfaces: []*wifi.Interface{
+				{Name: "wlan0", HardwareAddr: parseMAC("00:11:22:33:44:55")},
+				{Name: "eth0", HardwareAddr: parseMAC("aa:bb:cc:dd:ee:ff")},
+			},
+			mockReturnError: nil,
+			expectedNames:   []string{"wlan0", "eth0"},
+			expectError:     false,
+		},
+		{
+			name:                 "error from interfaces",
+			mockReturnInterfaces: nil,
+			mockReturnError:      ErrExpected,
+			expectedNames:        nil,
+			expectError:          true,
+		},
+		{
+			name:                 "empty interfaces",
+			mockReturnInterfaces: []*wifi.Interface{},
+			mockReturnError:      nil,
+			expectedNames:        []string{},
+			expectError:          false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			mockWiFi := NewWiFiHandle(t)
+			service := myWiFi.New(mockWiFi)
+
+			mockWiFi.On("Interfaces").Return(tc.mockReturnInterfaces, tc.mockReturnError)
+			names, err := service.GetNames()
+
+			if tc.expectError {
+				require.Error(t, err)
+				require.ErrorIs(t, err, tc.mockReturnError)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedNames, names)
+			}
+		})
+	}
+}
