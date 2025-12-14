a72285e90083ec745b7efed1a9d7ae8d89457284
TvoyBatyA12343 <maxdem06@gmail.com>
2025-12-14 18:27:04 +0300

    [TASK-6] all unique names tests

--- maxim.demehin/task-6/internal/db/db_test.go
+++ maxim.demehin/task-6/internal/db/db_test.go
@@ -1,6 +1,7 @@
 package db_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -12,6 +13,8 @@ const (
 	selectAllNames = "SELECT name FROM users"
 	selectUnique   = "SELECT DISTINCT name FROM users"
 )
+
+var TestError = errors.New("test error")
 
 func TestNew(t *testing.T) {
 	t.Parallel()
@@ -39,4 +42,58 @@ func TestGetUniqueNames_Success(t *testing.T) {
 	names, err := service.GetUniqueNames()
 	require.NoError(t, err)
 	require.Equal(t, []string{"Alex", "Maria"}, names)
+}
+
+func TestGetUniqueNames_QueryFailure(t *testing.T) {
+	t.Parallel()
+
+	mockDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer mockDB.Close()
+
+	service := db.DBService{DB: mockDB}
+
+	mock.ExpectQuery(selectUnique).WillReturnError(TestError)
+
+	names, err := service.GetUniqueNames()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "db query")
+	require.Nil(t, names)
+}
+
+func TestGetUniqueNames_InvalidData(t *testing.T) {
+	t.Parallel()
+
+	mockDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer mockDB.Close()
+
+	service := db.DBService{DB: mockDB}
+
+	rows := sqlmock.NewRows([]string{"name"}).AddRow(nil)
+	mock.ExpectQuery(selectUnique).WillReturnRows(rows)
+
+	names, err := service.GetUniqueNames()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "rows scanning")
+	require.Nil(t, names)
+}
+
+func TestGetUniqueNames_RowIssue(t *testing.T) {
+	t.Parallel()
+
+	mockDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer mockDB.Close()
+
+	service := db.DBService{DB: mockDB}
+
+	rows := sqlmock.NewRows([]string{"name"}).AddRow("Peter")
+	rows.RowError(0, TestError)
+	mock.ExpectQuery(selectUnique).WillReturnRows(rows)
+
+	names, err := service.GetUniqueNames()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "rows error")
+	require.Nil(t, names)
 }
