e41af2de823f301d484a688d0b96aed564cbd155
MezentsevAnton <fortniter135.anton@mail.ru>
2025-12-14 17:34:36 +0300

    [TASK-8] is done

---
+++ anton.mezentsev/task-8/cmd/service/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"anton.mezentsev/task-8/config"
+)
+
+func main() {
+	config, err := config.ParseConfig()
+	if err != nil {
+		fmt.Println("Error: ", err)
+
+		return
+	}
+
+	fmt.Print(config.Environment, " ", config.LogLevel)
+}
