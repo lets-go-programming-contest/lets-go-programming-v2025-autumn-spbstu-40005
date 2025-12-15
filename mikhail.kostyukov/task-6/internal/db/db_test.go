d57518e48632cc3c4ebb71e41365e2aafd27ef82
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-12-15 21:52:23 +0300

    [TASK-6] Added TestGetNames for db

--- mikhail.kostyukov/task-6/internal/db/db_test.go
+++ mikhail.kostyukov/task-6/internal/db/db_test.go
@@ -1,6 +1,7 @@
 package db_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -8,6 +9,82 @@ import (
 
 	myDB "github.com/KostyukovMichael/lets-go-programming-v2025-autumn-spbstu-40005/task-6/internal/db"
 )
+
+var errExpected = errors.New("expeted error")
+
+func TestGetNames(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name          string
+		mockSetup     func(sqlmock.Sqlmock)
+		expectedNames []string
+		expectError   bool
+		errorContains string
+	}{
+		{
+			name: "success",
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"name"}).AddRow("Misha").AddRow("Masha")
+				mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
+			},
+			expectedNames: []string{"Misha", "Masha"},
+			expectError:   false,
+		},
+		{
+			name: "query error",
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("SELECT name FROM users").WillReturnError(errExpected)
+			},
+			expectedNames: nil,
+			expectError:   true,
+			errorContains: "db query",
+		},
+		{
+			name: "scan error",
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"name"}).AddRow(nil)
+				mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
+			},
+			expectedNames: nil,
+			expectError:   true,
+			errorContains: "rows scanning",
+		},
+		{
+			name: "rows iteration error",
+			mockSetup: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"name"}).AddRow("Misha").RowError(0, errExpected)
+				mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
+			},
+			expectedNames: nil,
+			expectError:   true,
+			errorContains: "rows error",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			db, mock, err := sqlmock.New()
+			require.NoError(t, err)
+			defer db.Close()
+
+			service := myDB.New(db)
+
+			tc.mockSetup(mock)
+
+			names, err := service.GetNames()
+
+			if tc.expectError {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.errorContains)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedNames, names)
+			}
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
 
 func TestNew(t *testing.T) {
 	t.Parallel()
