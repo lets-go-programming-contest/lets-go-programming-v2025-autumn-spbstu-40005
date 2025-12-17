d7b00609ade8f9d71bedc2a5f31b21b85df245a3
hahahapolinka <gavrilova.pv@edu.spbstu.ru>
2025-12-17 20:16:44 +0300

    [TASK-3] (#93)
    * [TASK-3] Initial commit
    
    * [TASK-3] Add config loading and basic structure
    
    * [TASK-3] Define models
    
    * [TASK-3] Implement XML reading
    
    * [TASK-3] Implement sorting and writing JSON
    
    * [TASK-3] Final edits in main
    
    * [TASK-3] Fix: correct XML encoding and file handling
    
    * [TASK-3] Fix: use Go 1.22 compatible dependencies
    
    * [TASK-3] Fix work with errors
    
    * [TASK-3] Fix lint
    
    * [TASK-3] Fix sorting
    
    * [TASK-3] Fix lint
    
    * [TASK-3] Fix models
    
    * [TASK-3] Fix models
    
    * [TASK-3] Fix sorting
    
    * [TASK-3] Fix lint
    
    * [TASK-3] Permissions to constants
    
    * [TASK-3] Permissions to constants
    
    * [TASK-3] internal/parser + some edits
    
    * [TASK-3] lint
    
    * [TASK-3] lint
    
    * [TASK-3] lint
    
    * [TASK-3] lint
    
    * [TASK-3] lint
    
    ---------
    
    Co-authored-by: hahaha_polina <polina.gavrilova2022@mail.ru>

---
+++ polina.gavrilova/task-3/internal/processor/processor.go
@@ -0,0 +1,68 @@
+package processor
+
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"polina.gavrilova/task-3/internal/config"
+	"polina.gavrilova/task-3/internal/models"
+	"polina.gavrilova/task-3/internal/parser"
+)
+
+const (
+	DefaultDirPermissions  = 0o755
+	DefaultFilePermissions = 0o600
+)
+
+func Run(cfg *config.Config) error {
+	xmlData, err := parser.ReadXMLData(cfg.InputFile)
+	if err != nil {
+		return fmt.Errorf("error reading xml file: %w", err)
+	}
+
+	valutes := transformAndSort(xmlData)
+
+	dirPerm := getDirPermissions(cfg)
+	filePerm := getFilePermissions(cfg)
+
+	err = parser.WriteJSONData(cfg.OutputFile, valutes, dirPerm, filePerm)
+	if err != nil {
+		return fmt.Errorf("error writing json file: %w", err)
+	}
+
+	return nil
+}
+
+func getDirPermissions(cfg *config.Config) os.FileMode {
+	if cfg.DirPerms != nil {
+		return os.FileMode(*cfg.DirPerms)
+	}
+
+	return DefaultDirPermissions
+}
+
+func getFilePermissions(cfg *config.Config) os.FileMode {
+	if cfg.FilePerms != nil {
+		return os.FileMode(*cfg.FilePerms)
+	}
+
+	return DefaultFilePermissions
+}
+
+func transformAndSort(xmlData *models.ValCurs) []models.Valute {
+	valutes := make([]models.Valute, len(xmlData.Valutes))
+	for i, v := range xmlData.Valutes {
+		valutes[i] = models.Valute{
+			NumCode:  v.NumCode,
+			CharCode: v.CharCode,
+			Value:    v.Value,
+		}
+	}
+
+	sort.Slice(valutes, func(i, j int) bool {
+		return float64(valutes[i].Value) > float64(valutes[j].Value)
+	})
+
+	return valutes
+}
