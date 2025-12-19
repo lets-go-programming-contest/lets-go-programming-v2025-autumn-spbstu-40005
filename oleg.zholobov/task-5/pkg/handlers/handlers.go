43a4fb8011db7ae4cf624938336adc7ab8482656
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-12-19 18:57:28 +0300

    [TASK-5] fix linter (4)

--- oleg.zholobov/task-5/pkg/handlers/handlers.go
+++ oleg.zholobov/task-5/pkg/handlers/handlers.go
@@ -48,6 +48,7 @@ func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan str
 
 func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
 	if len(outputs) == 0 {
+
 		return errEmptyOutputs
 	}
 
