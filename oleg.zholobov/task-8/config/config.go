c711cd1e0ad676fb5aae38a3af167b0bcadea351
OlegZholobov <zholobov.of@edu.spbstu.ru>
2025-12-23 21:53:20 +0300

    [TASK-8] (#168)
    * [TASK-8] initial commit
    
    * [TASK-8] looks like done
    
    * [TASK-8] fix linter
    
    * [TASK-8] fix linter (2)
    
    * [TASK-8] fix test and linter (3)
    
    * [TASK-8] fix test
    
    * [TASK-8] changes per comments
    
    * [TASK-8] fix linter (3)

---
+++ oleg.zholobov/task-8/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	LogLevel    string `yaml:"log_level"`
+}
+
+func (c *Config) String() string {
+	return fmt.Sprintf("%s %s", c.Environment, c.LogLevel)
+}
+
+func LoadConfig() (*Config, error) {
+	cfg := &Config{
+		Environment: "",
+		LogLevel:    "",
+	}
+
+	err := yaml.Unmarshal(configData, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	return cfg, nil
+}
