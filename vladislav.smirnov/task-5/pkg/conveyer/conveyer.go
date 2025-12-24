25f68aba088678c6b116aaadd9267db8a9d47a79
smirnov-vladislav <smirnovvladislav505@gmail.com>
2025-12-24 23:59:42 +0300

    [TASK-5] (#131)
    * [TASK-5] initial commit
    
    * [TASK-5] add go.sum file
    
    * [TASK-5] forgot to change version
    
    * [TASK-5] little fix
    
    * [TASK-5] add handlers functions
    
    * [TASK-5] add empty lines
    
    * [TASK-5] change name of variable
    
    * [TASK-5] fixes
    
    * [TASK-5] fix registers
    
    * [TASK-5] problems with names
    
    * [TASK-5] fix linter
    
    * [TASK-5] one more fix linter
    
    * [TASK-5] use of interface conveyer
    
    * [TASK-5] change multiplexer
    
    * [TASK-5] fixes
    
    * [TASK-5] fix in handlers.go
    
    * [TASK-5] deleted interface.go
    
    * [TASK-5] changes
    
    * [TASK-5] last fix
    
    * [TASK-5] very last fix for my favourite linter
    
    * [TASK-5] last one
    
    * [TASK-5] jingle bells
    
    * [TASK-5] changes
    
    * [TASK-5] little fix
    
    * [TASK-5] made it easier
    
    * [TASK-5] fix
    
    * [TASK-5] split registers into several lines
    
    * [TASK-5] little fix last one
    
    * [TASK-5] change
    
    * [TASK-5] little fix
    
    * [TASK-5] fix linter

---
+++ vladislav.smirnov/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,183 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	ErrChanNotFound    = errors.New("chan not found")
+	ErrUnknownTask     = errors.New("unknown task")
+	ErrChannelFull     = errors.New("channel is full")
+	ErrInvalidTaskFunc = errors.New("invalid task function")
+)
+
+const undefined = "undefined"
+
+type task func(context.Context) error
+
+type Conveyer struct {
+	channels map[string]chan string
+	size     int
+	tasks    []task
+	mu       sync.RWMutex
+}
+
+func New(size int) *Conveyer {
+	return &Conveyer{
+		channels: make(map[string]chan string),
+		size:     size,
+		tasks:    []task{},
+		mu:       sync.RWMutex{},
+	}
+}
+
+func (c *Conveyer) getChannel(name string) (chan string, error) {
+	c.mu.RLock()
+
+	channel, found := c.channels[name]
+
+	c.mu.RUnlock()
+
+	if !found {
+		return nil, ErrChanNotFound
+	}
+
+	return channel, nil
+}
+
+func (c *Conveyer) createChannel(name string) chan string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	channel, found := c.channels[name]
+
+	if found {
+		return channel
+	}
+
+	channel = make(chan string, c.size)
+	c.channels[name] = channel
+
+	return channel
+}
+
+func (c *Conveyer) closeAllChannels() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, channel := range c.channels {
+		close(channel)
+	}
+}
+
+func (c *Conveyer) Run(ctx context.Context) error {
+	defer c.closeAllChannels()
+
+	errGroup, gCtx := errgroup.WithContext(ctx)
+
+	c.mu.RLock()
+
+	for _, task := range c.tasks {
+		currentTask := task
+
+		errGroup.Go(func() error {
+			return currentTask(gCtx)
+		})
+	}
+
+	c.mu.RUnlock()
+
+	err := errGroup.Wait()
+	if err != nil {
+		return fmt.Errorf("run tasks failed: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Conveyer) Send(name, data string) error {
+	channel, err := c.getChannel(name)
+	if err != nil {
+		return err
+	}
+
+	select {
+	case channel <- data:
+		return nil
+	default:
+		return ErrChannelFull
+	}
+}
+
+func (c *Conveyer) Recv(name string) (string, error) {
+	channel, err := c.getChannel(name)
+	if err != nil {
+		return "", ErrChanNotFound
+	}
+
+	val, ok := <-channel
+
+	if !ok {
+		return undefined, nil
+	}
+
+	return val, nil
+}
+
+func (c *Conveyer) RegisterDecorator(
+	decFunc func(context.Context, chan string, chan string) error,
+	input string,
+	output string,
+) {
+	inChannel := c.createChannel(input)
+	outChannel := c.createChannel(output)
+
+	c.mu.Lock()
+	c.tasks = append(c.tasks, func(ctx context.Context) error {
+		return decFunc(ctx, inChannel, outChannel)
+	})
+	c.mu.Unlock()
+}
+
+func (c *Conveyer) RegisterMultiplexer(
+	muxFunc func(context.Context, []chan string, chan string) error,
+	inputs []string,
+	output string,
+) {
+	ins := make([]chan string, len(inputs))
+	for i, name := range inputs {
+		ins[i] = c.createChannel(name)
+	}
+
+	outChannel := c.createChannel(output)
+
+	c.mu.Lock()
+	c.tasks = append(c.tasks, func(ctx context.Context) error {
+		return muxFunc(ctx, ins, outChannel)
+	})
+	c.mu.Unlock()
+}
+
+func (c *Conveyer) RegisterSeparator(
+	sepFunc func(context.Context, chan string, []chan string) error,
+	input string,
+	outputs []string,
+) {
+	inChannel := c.createChannel(input)
+	outs := make([]chan string, len(outputs))
+
+	for i, name := range outputs {
+		outs[i] = c.createChannel(name)
+	}
+
+	c.mu.Lock()
+	c.tasks = append(c.tasks, func(ctx context.Context) error {
+		return sepFunc(ctx, inChannel, outs)
+	})
+	c.mu.Unlock()
+}
