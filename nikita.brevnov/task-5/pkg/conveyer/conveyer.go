2233d12580cff9827c5ef7a61ed9cf61acb03263
Brev1ch <flirktv@gmail.com>
2025-12-25 00:14:00 +0300

    [TASK-5] (#130)
    * [TASK-5] Initial commit
    
    * [TASK-5] conveyor add
    
    * [TASK-5] handler add
    
    * [TASK-5] enter fix
    
    * [TASK-5] Renamed
    
    * [TASK-5] handlers
    
    * [TASK-5] conveyers
    
    * [TASK-5] handler.go
    
    * [TASK-5] wrongs fix
    
    * [TASK-5] Big fixes
    
    * [TASK-5] Testfix
    
    * [TASK-5] conveyer fix
    
    * [TASK-5] Tests fix
    
    * [TASK-5] Linter fix
    
    * [TASK-5] ok
    
    * [TASK-5] first fix
    
    * [TASK-5] second fix
    
    * [TASK-5] Linter
    
    * [TASK-5] return fix
    
    * [TASK-5] third fix
    
    * [TASK-5] return fix
    
    * [TASK-5] return fix
    
    * [TASK-5] fourth fix
    
    * [TASK-5] linter fix
    
    * [TASK-5] conveyer fix

---
+++ nikita.brevnov/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,163 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+const undefinedValue = "undefined"
+
+var ErrChannelNotFound = errors.New("chan not found")
+
+type workerFunc func(ctx context.Context) error
+
+type conveyer struct {
+	bufferSize int
+	channels   map[string]chan string
+	workers    []workerFunc
+	mutex      sync.RWMutex
+}
+
+func New(size int) *conveyer {
+	return &conveyer{
+		bufferSize: size,
+		channels:   make(map[string]chan string),
+		workers:    make([]workerFunc, 0),
+		mutex:      sync.RWMutex{},
+	}
+}
+
+func (c *conveyer) RegisterDecorator(
+	processor func(ctx context.Context, in chan string, out chan string) error,
+	inName,
+	outName string,
+) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	inChan := c.getChannelOrCreate(inName)
+	outChan := c.getChannelOrCreate(outName)
+
+	c.workers = append(c.workers, func(ctx context.Context) error {
+		return processor(ctx, inChan, outChan)
+	})
+}
+
+func (c *conveyer) RegisterMultiplexer(
+	processor func(ctx context.Context, ins []chan string, out chan string) error,
+	inNames []string,
+	outName string,
+) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	outChan := c.getChannelOrCreate(outName)
+	inChans := make([]chan string, len(inNames))
+
+	for i, name := range inNames {
+		inChans[i] = c.getChannelOrCreate(name)
+	}
+
+	c.workers = append(c.workers, func(ctx context.Context) error {
+		return processor(ctx, inChans, outChan)
+	})
+}
+
+func (c *conveyer) RegisterSeparator(
+	processor func(ctx context.Context, in chan string, outs []chan string) error,
+	inName string,
+	outNames []string,
+) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	inChan := c.getChannelOrCreate(inName)
+	outChans := make([]chan string, len(outNames))
+
+	for i, name := range outNames {
+		outChans[i] = c.getChannelOrCreate(name)
+	}
+
+	c.workers = append(c.workers, func(ctx context.Context) error {
+		return processor(ctx, inChan, outChans)
+	})
+}
+
+func (c *conveyer) Run(ctx context.Context) error {
+	defer c.closeAll()
+
+	errGroup, ctx := errgroup.WithContext(ctx)
+
+	for _, w := range c.workers {
+		errGroup.Go(func() error {
+			return w(ctx)
+		})
+	}
+
+	if err := errGroup.Wait(); err != nil {
+		return fmt.Errorf("execution failed: %w", err)
+	}
+
+	return nil
+}
+
+func (c *conveyer) closeAll() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	for _, ch := range c.channels {
+		close(ch)
+	}
+}
+
+func (c *conveyer) getChannel(name string) (chan string, error) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
+	ch, ok := c.channels[name]
+	if !ok {
+		return nil, ErrChannelNotFound
+	}
+
+	return ch, nil
+}
+
+func (c *conveyer) Send(name string, data string) error {
+	ch, exists := c.getChannel(name)
+	if exists != nil {
+		return ErrChannelNotFound
+	}
+
+	ch <- data
+
+	return nil
+}
+
+func (c *conveyer) Recv(name string) (string, error) {
+	ch, exists := c.getChannel(name)
+	if exists != nil {
+		return "", ErrChannelNotFound
+	}
+
+	value, ok := <-ch
+	if !ok {
+		return undefinedValue, nil
+	}
+
+	return value, nil
+}
+
+func (c *conveyer) getChannelOrCreate(name string) chan string {
+	if ch, ok := c.channels[name]; ok {
+		return ch
+	}
+
+	ch := make(chan string, c.bufferSize)
+	c.channels[name] = ch
+
+	return ch
+}
