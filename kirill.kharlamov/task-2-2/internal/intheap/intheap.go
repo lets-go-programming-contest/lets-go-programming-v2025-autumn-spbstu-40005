a413344dec46a23b4c7b079ed0ac72ff11286bb1
0Revaivel0 <kharlamov_k1999@bk.ru>
2025-12-30 20:18:08 +0300

    [TASK-2-2] fix oshibok

--- kirill.kharlamov/task-2-2/internal/intheap/intheap.go
+++ kirill.kharlamov/task-2-2/internal/intheap/intheap.go
@@ -1,6 +1,9 @@
 package intheap
 
-import "container/heap"
+import (
+	"container/heap"
+	"fmt"
+)
 
 type CustomHeap []int
 
@@ -9,23 +12,33 @@ func (h *CustomHeap) Len() int {
 }
 
 func (h *CustomHeap) Less(i, j int) bool {
+	if i >= h.Len() || j >= h.Len() {
+		panic(fmt.Sprintf("index out of range: i=%d, j=%d, len=%d", i, j, h.Len()))
+	}
 	return (*h)[i] < (*h)[j]
 }
 
 func (h *CustomHeap) Swap(i, j int) {
+	if i >= h.Len() || j >= h.Len() {
+		panic(fmt.Sprintf("index out of range: i=%d, j=%d, len=%d", i, j, h.Len()))
+	}
 	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *CustomHeap) Push(x interface{}) {
 	value, correctType := x.(int)
 	if !correctType {
-		return
+		panic(fmt.Sprintf("invalid type: expected int, got %T", x))
 	}
 
 	*h = append(*h, value)
 }
 
 func (h *CustomHeap) Pop() interface{} {
+	if h.Len() == 0 {
+		panic("pop from empty heap")
+	}
+
 	oldSlice := *h
 	n := len(oldSlice)
 	lastElement := oldSlice[n-1]
@@ -35,6 +48,10 @@ func (h *CustomHeap) Pop() interface{} {
 }
 
 func FindKthPreference(ratings []int, k int) int {
+	if len(ratings) == 0 || k <= 0 || k > len(ratings) {
+		panic(fmt.Sprintf("invalid parameters: ratings len=%d, k=%d", len(ratings), k))
+	}
+
 	heapContainer := &CustomHeap{}
 	heap.Init(heapContainer)
 
