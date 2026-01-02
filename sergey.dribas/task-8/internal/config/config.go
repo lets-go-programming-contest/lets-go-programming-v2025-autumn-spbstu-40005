8ef09f9b44dc72e468f58e0371c6f30774b42c14
Rassoha <serdribas@gmail.com>
2026-01-02 23:15:01 +0300

    [TASK-8] initital commit

---
+++ sergey.dribas/task-8/internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	LogLevel    string `yaml:"log_level"`
+}
+
+func GetConfig() (*Config, error) {
+	var cnfg Config
+
+	err := yaml.Unmarshal(dataFile, &cnfg)
+	if err != nil {
+		return nil, fmt.Errorf("error in parsing config: %w", err)
+	}
+
+	return &cnfg, nil
+}
+
+func (c *Config) PrintToConfig() {
+	fmt.Printf("%s %s", c.Environment, c.LogLevel)
+}
