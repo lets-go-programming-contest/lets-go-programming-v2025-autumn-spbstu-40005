402ed6fc7f31206b68abd31450efcfdb89445d13
Robotophile <ksyunya.iv@gmail.com>
2026-01-04 18:12:18 +0300

    [TASK-3] model

---
+++ ksenia.ivanova/task-3/internal/model/model.go
@@ -0,0 +1,69 @@
+package model
+
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type CurrencyValue float64
+
+func (v *CurrencyValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var str string
+	if err := d.DecodeElement(&str, &start); err != nil {
+		return fmt.Errorf("failed to unmarshal currency value: %w", err)
+	}
+
+	str = strings.ReplaceAll(str, ",", ".")
+
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return fmt.Errorf("failed to parse currency value '%s': %w", str, err)
+	}
+
+	*v = CurrencyValue(value)
+	return nil
+}
+
+func (v CurrencyValue) MarshalJSON() ([]byte, error) {
+	str := strconv.FormatFloat(float64(v), 'f', 4, 64)
+	str = strings.ReplaceAll(str, ".", ",")
+	return []byte(`"` + str + `"`), nil
+}
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Date    string   `xml:"Date,attr"`
+	Name    string   `xml:"name,attr"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+	ID       string        `xml:"ID,attr"`
+	NumCode  string        `xml:"ISO_Num_Code"`
+	CharCode string        `xml:"ISO_Char_Code"`
+	Nominal  int           `xml:"Nominal"`
+	Name     string        `xml:"Name"`
+	Value    CurrencyValue `xml:"Value"`
+}
+
+type OutputCurrency struct {
+	NumCode  string        `json:"iso_num_code"`
+	CharCode string        `json:"iso_char_code"`
+	Value    CurrencyValue `json:"value"`
+}
+
+type ByValueDesc []Valute
+
+func (v ByValueDesc) Len() int {
+	return len(v)
+}
+
+func (v ByValueDesc) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+func (v ByValueDesc) Less(i, j int) bool {
+	return float64(v[i].Value) > float64(v[j].Value)
+}
