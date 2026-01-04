219b97ecfd0042990ab82edd04c230cf80314360
Robotophile <ksyunya.iv@gmail.com>
2026-01-04 18:28:26 +0300

    [TASK-3] main and fixes

--- ksenia.ivanova/task-3/cmd/service/main.go
+++ ksenia.ivanova/task-3/cmd/service/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"flag"
+	"sort"
+
+	"ksenia.ivanova/task-3/internal/config"
+	"ksenia.ivanova/task-3/internal/converter"
+	"ksenia.ivanova/task-3/internal/model"
+)
+
+const (
+	configFlagName    = "config"
+	configFlagDefault = "config.yaml"
+	configFlagUsage   = "path to config file"
+)
+
+func main() {
+	configPath := flag.String(configFlagName, configFlagDefault, configFlagUsage)
+	flag.Parse()
+
+	appConfig, err := config.Load(*configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var rates model.ValCurs
+	if err := converter.ParseXMLFile(appConfig.InputFile, &rates); err != nil {
+		panic(err)
+	}
+
+	sort.Sort(model.ByValueDesc(rates.Valutes))
+
+	outputCurrencies := make([]model.OutputCurrency, len(rates.Valutes))
+	for i, valute := range rates.Valutes {
+		outputCurrencies[i] = model.OutputCurrency{
+			NumCode:  valute.NumCode,
+			CharCode: valute.CharCode,
+			Value:    valute.Value,
+		}
+	}
+
+	if err := converter.WriteToJSON(outputCurrencies, appConfig.OutputFile); err != nil {
+		panic(err)
+	}
+}
