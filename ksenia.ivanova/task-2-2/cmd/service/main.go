e1ace0645a143b0f35517d21e6e54874e89a8df5
Robotophile <ksyunya.iv@gmail.com>
2026-01-04 17:34:25 +0300

    [TASK-2-2] fix

--- ksenia.ivanova/task-2-2/cmd/service/main.go
+++ ksenia.ivanova/task-2-2/cmd/service/main.go
@@ -12,7 +12,9 @@ func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
 func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
 func (h *MinHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+	if val, ok := x.(int); ok {
+		*h = append(*h, val)
+	}
 }
 
 func (h *MinHeap) Pop() interface{} {
@@ -20,39 +22,49 @@ func (h *MinHeap) Pop() interface{} {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
 func main() {
-	var n, k int
+	var dishCount, kValue int
 
-	fmt.Scan(&n)
-
-	ratings := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&ratings[i])
-	}
-
-	fmt.Scan(&k)
-
-	if k < 1 || k > n {
+	_, err := fmt.Scan(&dishCount)
+	if err != nil {
 		return
 	}
 
-	h := &MinHeap{}
-	heap.Init(h)
-
-	for i := 0; i < k; i++ {
-		heap.Push(h, ratings[i])
-	}
-
-	for i := k; i < n; i++ {
-		if ratings[i] > (*h)[0] {
-			heap.Pop(h)
-			heap.Push(h, ratings[i])
+	ratings := make([]int, dishCount)
+	for index := range ratings {
+		_, err = fmt.Scan(&ratings[index])
+		if err != nil {
+			return
 		}
 	}
 
-	result := (*h)[0]
+	_, err = fmt.Scan(&kValue)
+	if err != nil {
+		return
+	}
+
+	if kValue < 1 || kValue > dishCount {
+		return
+	}
+
+	minHeap := &MinHeap{}
+	heap.Init(minHeap)
+
+	for index := 0; index < kValue; index++ {
+		heap.Push(minHeap, ratings[index])
+	}
+
+	for index := kValue; index < dishCount; index++ {
+		if ratings[index] > (*minHeap)[0] {
+			heap.Pop(minHeap)
+			heap.Push(minHeap, ratings[index])
+		}
+	}
+
+	result := (*minHeap)[0]
 	fmt.Println(result)
 }
