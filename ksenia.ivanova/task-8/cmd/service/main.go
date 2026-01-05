54c4058c404e8e5cf2d3c8eebc87ce604d709f86
Robotophile <ksyunya.iv@gmail.com>
2026-01-05 16:38:32 +0300

    [TASK-8] code

--- ksenia.ivanova/task-8/cmd/service/main.go
+++ ksenia.ivanova/task-8/cmd/service/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"ksenia.ivanova/task-8/internal/config"
+)
+
+func main() {
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	fmt.Printf("%s %s", cfg.Env, cfg.Log)
+}
