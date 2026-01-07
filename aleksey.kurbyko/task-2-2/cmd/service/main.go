a7fd33aa9f1d1284601bd667c369246e71b93a12
lechat222 <kurbyko2006@gmail.com>
2026-01-07 19:42:26 +0300

    [TASK-2-2] fix lint

--- aleksey.kurbyko/task-2-2/cmd/service/main.go
+++ aleksey.kurbyko/task-2-2/cmd/service/main.go
@@ -21,6 +21,7 @@ var (
 	ErrIncorrectRating    = errors.New("incorrect rating for the dish")
 	ErrIncorrectK         = errors.New("incorrect k")
 	ErrExpectedInt        = errors.New("expected int")
+	ErrEmptyHeap          = errors.New("empty heap")
 )
 
 func readInt() (int, error) {
@@ -33,49 +34,89 @@ func readInt() (int, error) {
 	return value, nil
 }
 
-func run() error {
+func readDishCount() (int, error) {
 	dishCount, err := readInt()
 	if err != nil {
-		return ErrIncorrectDishCount
+		return 0, ErrIncorrectDishCount
 	}
 
 	if dishCount < DishCountMin || dishCount > DishCountMax {
-		return ErrIncorrectDishCount
+		return 0, ErrIncorrectDishCount
 	}
 
+	return dishCount, nil
+}
+
+func readPreferredIndex(dishCount int) (int, error) {
+	preferredIndex, err := readInt()
+	if err != nil {
+		return 0, ErrIncorrectK
+	}
+
+	if preferredIndex < 1 || preferredIndex > dishCount {
+		return 0, ErrIncorrectK
+	}
+
+	return preferredIndex, nil
+}
+
+func buildRatingsHeap(dishCount int) (*dishheap.DishHeap, error) {
 	ratingsHeap := &dishheap.DishHeap{}
 	heap.Init(ratingsHeap)
 
 	for range dishCount {
 		rating, err := readInt()
 		if err != nil {
-			return ErrIncorrectRating
+			return nil, ErrIncorrectRating
 		}
 
 		if rating < RatingMin || rating > RatingMax {
-			return ErrIncorrectRating
+			return nil, ErrIncorrectRating
 		}
 
 		heap.Push(ratingsHeap, rating)
 	}
 
-	k, err := readInt()
-	if err != nil {
-		return ErrIncorrectK
-	}
+	return ratingsHeap, nil
+}
 
-	if k < 1 || k > dishCount {
-		return ErrIncorrectK
-	}
-
-	for range k - 1 {
+func getPreferredRating(ratingsHeap *dishheap.DishHeap, preferredIndex int) (int, error) {
+	for range preferredIndex - 1 {
 		heap.Pop(ratingsHeap)
 	}
 
-	resultAny := heap.Pop(ratingsHeap)
-	result, ok := resultAny.(int)
+	popped := heap.Pop(ratingsHeap)
+	if popped == nil {
+		return 0, ErrEmptyHeap
+	}
+
+	result, ok := popped.(int)
 	if !ok {
-		return ErrExpectedInt
+		return 0, ErrExpectedInt
+	}
+
+	return result, nil
+}
+
+func run() error {
+	dishCount, err := readDishCount()
+	if err != nil {
+		return err
+	}
+
+	ratingsHeap, err := buildRatingsHeap(dishCount)
+	if err != nil {
+		return err
+	}
+
+	preferredIndex, err := readPreferredIndex(dishCount)
+	if err != nil {
+		return err
+	}
+
+	result, err := getPreferredRating(ratingsHeap, preferredIndex)
+	if err != nil {
+		return err
 	}
 
 	fmt.Println(result)
