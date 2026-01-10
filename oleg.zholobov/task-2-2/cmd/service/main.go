ba08a5f45cc7afc825073d7d31dd223c821d69b3
OlegZholobov <zholobov.of@edu.spbstu.ru>
2026-01-10 11:01:52 +0300

    [TASK-2-2] (#199) 1/3
    * [TASK-2-2] initial commit
    
    * [TASK-2-2] made input
    
    * [TASK-2-2] added heap
    
    * [TASK-2-2] beta
    
    * [TASK-2-2] pre-release
    
    * [TASK-2-2] fix linter
    
    * [TASK-2-2] fix linter 2
    
    * [TASK-2-2] fix linter 3
    
    * [TASK-2-2] changes per review
    
    * [TASK-2-2] fix linter 4
    
    * [TASK-2-2] final fix
    
    * [TASK-2-2] changes per review (2)
    
    * [TASK-2-2] fix linter (5)

---
+++ oleg.zholobov/task-2-2/cmd/service/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"container/heap"
+	"errors"
+	"fmt"
+)
+
+type MinHeap []int
+
+func (heap *MinHeap) Len() int {
+	return len(*heap)
+}
+
+func (heap *MinHeap) Less(i, j int) bool {
+	if i < 0 || i >= heap.Len() || j < 0 || j >= heap.Len() {
+		panic(fmt.Sprintf("MinHeap.Less: indices out of range: i=%d, j=%d, len=%d", i, j, heap.Len()))
+	}
+
+	return (*heap)[i] < (*heap)[j]
+}
+
+func (heap *MinHeap) Swap(i, j int) {
+	if i < 0 || i >= heap.Len() || j < 0 || j >= heap.Len() {
+		panic(fmt.Sprintf("MinHeap.Swap: indices out of range: i=%d, j=%d, len=%d", i, j, heap.Len()))
+	}
+
+	(*heap)[i], (*heap)[j] = (*heap)[j], (*heap)[i]
+}
+
+func (heap *MinHeap) Push(x any) {
+	v, ok := x.(int)
+	if !ok {
+		panic(fmt.Sprintf("MinHeap.Push: expected int, got %T", x))
+	}
+
+	*heap = append(*heap, v)
+}
+
+func (heap *MinHeap) Pop() any {
+	if heap.Len() == 0 {
+		return nil
+	}
+
+	old := *heap
+	length := len(old)
+	x := old[length-1]
+	*heap = old[0 : length-1]
+
+	return x
+}
+
+var ErrHeapEmpty = errors.New("heap is empty")
+
+func (heap *MinHeap) Peek() (int, error) {
+	if heap.Len() == 0 {
+		return 0, ErrHeapEmpty
+	}
+
+	return (*heap)[0], nil
+}
+
+func main() {
+	var amountOfDishes int
+	if _, err := fmt.Scan(&amountOfDishes); err != nil {
+		fmt.Println("Error: invalid format for number of dishes")
+
+		return
+	}
+
+	if amountOfDishes <= 0 {
+		fmt.Println("Error: number of dishes must be a positive number")
+
+		return
+	}
+
+	dishes := make([]int, amountOfDishes)
+	for i := range dishes {
+		if _, err := fmt.Scan(&dishes[i]); err != nil {
+			fmt.Println("Error: invalid format for dish rating")
+
+			return
+		}
+	}
+
+	var dishNumber int
+	if _, err := fmt.Scan(&dishNumber); err != nil {
+		fmt.Println("Error: invalid format for dish number")
+
+		return
+	}
+
+	if dishNumber <= 0 {
+		fmt.Println("Error: dish number must be a positive number")
+
+		return
+	}
+
+	if dishNumber > amountOfDishes {
+		fmt.Printf("Error: dish number (%d) cannot exceed number of dishes (%d)\n", dishNumber, amountOfDishes)
+
+		return
+	}
+
+	dishesHeap := &MinHeap{}
+	heap.Init(dishesHeap)
+
+	for _, val := range dishes {
+		heap.Push(dishesHeap, val)
+
+		if dishesHeap.Len() > dishNumber {
+			heap.Pop(dishesHeap)
+		}
+	}
+
+	result, err := dishesHeap.Peek()
+	if err != nil {
+		fmt.Println("No dish available")
+
+		return
+	}
+
+	fmt.Println(result)
+}
